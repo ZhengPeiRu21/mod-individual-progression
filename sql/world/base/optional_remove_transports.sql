43e4491aed3151d0b16350882994e45864ab7953
郑佩茹 <zhengpeiru21@protonmail.com>
2022-06-04 22:39:07 -0600

    Re-route The Bravery to Menethil Harbor

--- sql/world/base/optional_remove_transports.sql
+++ sql/world/base/optional_remove_transports.sql
@@ -1,6 +1,15 @@
-/* Removes transports added in WotLK that remove separation between the continents in early game. */
--- From Stormwind to Auberdine
-DELETE FROM `transports` WHERE `guid`=2;
+/* Re-route The Bravery to follow the pre-WotLK path to Menethil Harbor - fix associated text */
+UPDATE `gameobject_template` SET `Data0` = 295 WHERE `entry` = 176310;
+UPDATE `transports` SET `name` = 'Menethil Harbor and Auberdine, Darkshore ("Ship (The Bravery)")' WHERE `guid` = 2;
+UPDATE `npc_text` SET `text0_1` = 'We\'ve sworn to do our very best to protect the passengers of The Bravery. The sea lane between Auberdine and Menethil Harbor must remain safe.' WHERE `ID` = 12270;
+UPDATE `npc_text` SET `text0_1` = 'No ship currently sails from this dock. If you''d like to reach Auberdine, The Bravery sails from Menethil Harbor.' WHERE `ID` = 13321;
+UPDATE `npc_text` SET `text0_0` = 'On the northern dock, you can board a ship that will carry you to Rut''theran Village and Darnassus. From the southern dock, you can find passage across the Great Sea to Menethil Harbor on Khaz Modan. The dock to the west, at the end of the pier, leads to Azuremyst Isle, near the Exodar. Safe journeys to you!' WHERE `ID` = 5480;
 
+UPDATE `broadcast_text` SET `FemaleText` = 'We\'ve sworn to do our very best to protect the passengers of The Bravery. The sea lane between Auberdine and Menethil Harbor must remain safe.' WHERE `ID` = 24118;
+UPDATE `broadcast_text` SET `MaleText` = 'On the northern dock, you can board a ship that will carry you to Rut''theran Village and Darnassus. From the southern dock, you can find passage across the Great Sea to Menethil Harbor on Khaz Modan. The dock to the west, at the end of the pier, leads to Azuremyst Isle, near the Exodar. Safe journeys to you!' WHERE `ID` = 8106;
+UPDATE `broadcast_text` SET `FemaleText` = 'No ship currently sails from this dock. If you''d like to reach Auberdine, The Bravery sails from Menethil Harbor.' WHERE `ID` = 28636;
+
+
+/* Removes a transport added in late WotLK (3.2.0) that remove separation between the continents in early game. */
 -- From Orgrimmar to Thunder Bluff
 DELETE FROM `transports` WHERE `guid`=20;
