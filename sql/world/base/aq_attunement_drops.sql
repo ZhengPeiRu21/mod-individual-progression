8789bff4899fea203cc0f9eaab34524f2be2c028
郑佩茹 <zhengpeiru21@protonmail.com>
2022-09-06 17:36:29 -0600

    Fix Nightmare Corruption drop rates

--- sql/world/base/aq_attunement_drops.sql
+++ sql/world/base/aq_attunement_drops.sql
@@ -1,7 +1,7 @@
 -- Fix drop rate for Nightmare_corruption
-UPDATE `creature_loot_template` SET `Chance` = 1 WHERE `Item` = 21146;
-UPDATE `creature_loot_template` SET `Chance` = 1 WHERE `Item` = 21147;
-UPDATE `creature_loot_template` SET `Chance` = 1 WHERE `Item` = 21148;
+UPDATE `creature_loot_template` SET `Chance` = 5 WHERE `Item` = 21146;
+UPDATE `creature_loot_template` SET `Chance` = 5 WHERE `Item` = 21147;
+UPDATE `creature_loot_template` SET `Chance` = 5 WHERE `Item` = 21148;
 
 -- FIx drop rate for Narain's Scrying Goggles
 UPDATE `creature_loot_template` SET `Chance` = 2 WHERE `Item` = 20951;
