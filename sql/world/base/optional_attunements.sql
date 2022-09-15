b039630349a979bbb34392641d0fbc2a09244cd9
郑佩茹 <zhengpeiru21@protonmail.com>
2022-09-15 15:58:27 -0600

    Move Zul'gurub attunement to optional file

---
+++ sql/world/base/optional_attunements.sql
@@ -0,0 +1,2 @@
+/* These attunements are not in the original game, but make progression feel more smooth by requiring players to complete necessary story questlines before entering relevant dungeons */
+REPLACE INTO `dungeon_access_requirements` (`dungeon_access_id`, `requirement_type`, `requirement_id`, `requirement_note`, `faction`, `priority`, `leader_only`, `comment`) VALUES (20, 1, 8182, 'You must complete the quest "The Hand of Rastakhan" and be level 60 before entering Zul\'Gurub.', 2, NULL, 0, NULL);
