170c2443ce9958c3f69dbc12e1bc704214ceafa7
郑佩茹 <zhengpeiru21@protonmail.com>
2022-09-22 19:01:36 -0600

    Add explanation to AQ Quest Nerf SQL

--- sql/world/base/zz_optional_aq_quest_nerf.sql
+++ sql/world/base/zz_optional_aq_quest_nerf.sql
@@ -1,4 +1,6 @@
-/* Optional but suggested change. Be sure to run AFTER vanilla_quest_reputations.sql.
+/* Optional but suggested change. The AQ quests originally was designed for a large group of players to farm items given to a chosen player to turn in.
+   This reduces the quest requirements to be more reasonably completable by a solo player or small group.
+   Be sure to run AFTER vanilla_quest_reputations.sql.
    Increases the Rep for The Hand of the Righteous by 40 times, so only about 5 turn-ins are necessary instead of 200.
    Feel free to adjust to taste - divide RewardFactionOverride1 by 100 to get the amount of rep that would be awarded */
 UPDATE quest_template SET RewardFactionOverride1=800000, RewardFactionOverride2=0,RewardFactionOverride3=0,RewardFactionOverride4=0,RewardFactionOverride5=0 WHERE ID=8301;
