10cc05ae1b84cbf3e2186cae10e7c309489ee8cc
郑佩茹 <zhengpeiru21@protonmail.com>
2022-10-07 14:10:39 -0600

    WotLK pre-3.2 gems fix

--- sql/world/base/wotlk_vendors.sql
+++ sql/world/base/wotlk_vendors.sql
@@ -1,7 +1,7 @@
 -- Restore vendors to use pre-3.2 gems
-UPDATE `npc_vendor` SET `item`='36919' WHERE  `entry`=32172 AND `item`=36918 AND `ExtendedCost`=2706;
-UPDATE `npc_vendor` SET `item`='36922' WHERE  `entry`=32172 AND `item`=36921 AND `ExtendedCost`=2706;
-UPDATE `npc_vendor` SET `item`='36925' WHERE  `entry`=32172 AND `item`=36924 AND `ExtendedCost`=2706;
-UPDATE `npc_vendor` SET `item`='36928' WHERE  `entry`=32172 AND `item`=36927 AND `ExtendedCost`=2484;
-UPDATE `npc_vendor` SET `item`='36931' WHERE  `entry`=32172 AND `item`=36930 AND `ExtendedCost`=2484;
-UPDATE `npc_vendor` SET `item`='36934' WHERE  `entry`=32172 AND `item`=36933 AND `ExtendedCost`=2484;
+UPDATE `npc_vendor` SET `item`=36918 WHERE `item`=36919;
+UPDATE `npc_vendor` SET `item`=36921 WHERE `item`=36922;
+UPDATE `npc_vendor` SET `item`=36924 WHERE `item`=36925;
+UPDATE `npc_vendor` SET `item`=36927 WHERE `item`=36928;
+UPDATE `npc_vendor` SET `item`=36930 WHERE `item`=36931;
+UPDATE `npc_vendor` SET `item`=36933 WHERE `item`=36934;
