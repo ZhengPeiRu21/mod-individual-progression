00f55ad0510b7e5ba6677344b9304207b6172bf9
郑佩茹 <zhengpeiru21@protonmail.com>
2022-12-28 14:32:30 -0700

    Fix SQL issue in class_trainers

--- sql/world/base/class_trainers.sql
+++ sql/world/base/class_trainers.sql
@@ -6,7 +6,7 @@ DELETE FROM `npc_trainer` WHERE `ID`=200009 AND `SpellID`=688;
 DELETE FROM `npc_trainer` WHERE `SpellID`=5502;
 
 -- Remove starting dual wield skill for rogues, add to trainers at level 10
-DELETE FROM `playercreateinfo_skills` WHERE `raceMask`=0 AND `classMask`=40 AND `skill`=118;
+DELETE FROM `playercreateinfo_skills` WHERE `skill`=118;
 INSERT INTO `playercreateinfo_skills` (`raceMask`, `classMask`, `skill`, `rank`, `comment`) VALUES (0, 32, 118, 0, 'Dual Wield');
 DELETE FROM `npc_trainer` WHERE `ID` = 200015 AND `SpellID` = 674;
 INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200015, 674, 300, 0, 0, 10);
