6d21aac6b094319d4584a2a853ca5dff8fcade47
郑佩茹 <zhengpeiru21@protonmail.com>
2023-03-13 11:41:35 -0600

    Fix Varimathras quests

--- sql/world/base/zone_undercity.sql
+++ sql/world/base/zone_undercity.sql
@@ -37,8 +37,68 @@ INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionTex
 (2849, 10, 0, 'A class trainer', 5338, 1, 1, 2848, 0, 0, 0, NULL, 0, 0),
 (2849, 11, 0, 'A profession trainer', 6635, 1, 1, 2847, 0, 0, 0, NULL, 0, 0);
 
+/* Wrathgate NPCs - just deleting for now, but maybe can implement a post-Wrathgate world state later */
+DELETE FROM `creature` WHERE `guid` IN (43466, 79263);
+
 /* Restore Varimathras */
 UPDATE `creature` SET `id1`=2425, `equipment_id`=0 WHERE `id1`=36273;
 
-/* Wrathgate NPCs - just deleting for now, but maybe can implement a post-Wrathgate world state later */
-DELETE FROM `creature` WHERE `guid` IN (43466, 79263);
+/* Restore correct quests for Varimathras */
+DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` IN (550, 6522, 6521, 1048, 5725);
+
+DELETE FROM `creature_questender` WHERE `id` = 36273 AND `quest` = 6144;
+DELETE FROM `creature_questender` WHERE `id` = 36273 AND `quest` = 14351;
+DELETE FROM `creature_questender` WHERE `id` = 36273 AND `quest` = 14352;
+DELETE FROM `creature_questender` WHERE `id` = 36273 AND `quest` = 14353;
+DELETE FROM `creature_questender` WHERE `id` = 36273 AND `quest` = 14355;
+DELETE FROM `creature_questender` WHERE `id` = 36273 AND `quest` = 14356;
+REPLACE INTO `creature_questender` (`id`, `quest`) VALUES (2425, 6144);
+REPLACE INTO `creature_questender` (`id`, `quest`) VALUES (2425, 14351);
+REPLACE INTO `creature_questender` (`id`, `quest`) VALUES (2425, 14352);
+REPLACE INTO `creature_questender` (`id`, `quest`) VALUES (2425, 14353);
+REPLACE INTO `creature_questender` (`id`, `quest`) VALUES (2425, 14355);
+REPLACE INTO `creature_questender` (`id`, `quest`) VALUES (2425, 14356);
+
+DELETE FROM `creature_queststarter` WHERE `id`=2215 AND `quest`=14351;
+REPLACE INTO `creature_questender` (`id`, `quest`) VALUES (2425, 550);
+
+DELETE FROM `creature_loot_template` WHERE `Entry`=4421 AND `Item`=49205 AND `Reference`=0 AND `GroupId`=0;
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (4421, 17008, 0, 100, 0, 1, 0, 1, 1, 'Charlga Razorflank - Small Scroll');
+
+UPDATE `item_template` SET `flags` = 2048 WHERE `entry` = 17008;
+UPDATE `item_template` SET `description` = 'To be opened by Lord Varimathras.' WHERE `entry` = 3701;
+
+UPDATE `quest_template`
+SET `LogDescription`  = "Bring Ambassador Malcin's Head to Varimathras in the Undercity.",
+    `QuestCompletionLog` = "Return to Varimathras at the Royal Quarter in the Undercity."
+WHERE `ID` = 6521;
+
+UPDATE `quest_template`
+SET `LogDescription`  = "Take Darthalia's Sealed Commendation to Varimathras in the Undercity.",
+    `QuestDescription` = "Because you fought with such valor and perseverance in the Battle of Hillsbrad, I have written this commendation, extolling your heroics in combat for the high command to recognize.$b$bTake this sealed commendation to Varimathras in the Undercity.  Go with pride, $c.",
+    `QuestCompletionLog` = "Return to Varimathras at the Royal Quarter in the Undercity."
+WHERE `ID` = 14351;
+
+UPDATE `quest_template`
+SET `LogDescription`  = "Take the Small Scroll to Varimathras in the Undercity.",
+    `QuestDescription` = "My Lady,$B$BYou know very well what it is we are offering. The opportunity for you to take the Barrens and the lands beyond is a great one. We simply want your aid in return. By aid we mean nothing more than your giving us the freedom to take action in the area.$B$BPlease, allow us to discuss this further. We have great power and are willing to share with those wise enough to join us. I will remain outside Razorfen Downs for the time being.$B$BBest Regards,$BAmbassador Malcin",
+    `QuestCompletionLog` = "Return to Varimathras at the Royal Quarter in the Undercity."
+WHERE `ID` = 14352;
+
+UPDATE `quest_template`
+SET `LogDescription`  = "Bring Ambassador Malcin's Head to Varimathras in the Undercity.",
+    `QuestDescription` = "Supposedly this Malcin is outside Razorfen Downs. There's no question - he has to die.$b$bMy contacts in Orgrimmar tell me their scouts have found signs of the Plague down there. The quilboar are showing signs of being plagued, too; they're much more powerful. Whatever the Scourge are doing down there needs to end. Now.$b$bFind this Malcin and kill him. Report back here when he's dead.",
+    `QuestCompletionLog` = "Return to Varimathras at the Royal Quarter in the Undercity."
+WHERE `ID` = 14353;
+
+UPDATE `quest_template`
+SET `LogDescription`  = "Kill High Inquisitor Whitemane, Scarlet Commander Mograine, Herod, the Scarlet Champion and Houndmaster Loksey and then report back to Varimathras in the Undercity.",
+    `QuestDescription` = "One of my duties while I'm stationed here is to help the Forsaken take care of their borders, and that, $c, is exactly what you're going to do for me.$b$bI might live and breathe, but the Scarlet Crusade will cut down my people as quickly as they will one of the undead. They're a problem here, a big one, and you're going to do something about it.$b$bGo to the Monastery and slay High Inquisitor Whitemane and her lieutenants.",
+    `QuestCompletionLog` = "Return to Varimathras at the Royal Quarter in the Undercity."
+WHERE `ID` = 14355;
+
+UPDATE `quest_template`
+SET `LogDescription`  = "Bring the books Spells of Shadow and Incantations from the Nether to Varimathras in Undercity.",
+    `QuestDescription` = "Listen up, $c.$b$bYou may not know this, but Orgrimmar's got a problem. Deep in Ragefire Chasm, a sect of the Shadow Council called the Searing Blade performs their sinister work. They're mainly orcs, and I can't trust sensitive information in the hands of the grunts there. That's where you come in.$b$bThey should have two books in their possession. I want them kept out of the hands of the Searing Blade and the Forsaken alike. Bring them to me directly.",
+    `QuestCompletionLog` = "Return to Varimathras at the Royal Quarter in the Undercity."
+WHERE `ID` = 14356;
