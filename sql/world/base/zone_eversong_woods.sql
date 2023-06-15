b0d7a07926762a5c245a8e163d0b73789e91120f
郑佩茹 <zhengpeiru21@protonmail.com>
2023-06-15 07:42:58 -0600

    Add missing quest drop for Windows to the Source

--- sql/world/base/zone_eversong_woods.sql
+++ sql/world/base/zone_eversong_woods.sql
@@ -5,3 +5,5 @@ INSERT INTO `creature_questender` (`id`, `quest`) VALUES (15283, 8344);
 -- Windows to the Source (Warlock)
 DELETE FROM `creature_queststarter` WHERE `id`=15283 AND `quest`=8344;
 INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES (15283, 8344);
+INSERT INTO creature_loot_template (Entry, Item, Reference, Chance, QuestRequired, LootMode, GroupId, MinCount, MaxCount, Comment) VALUES
+(15298, 20935, 0, 80, 1, 1, 0, 1, 1, 'Tainted Arcane Wraith - Tainted Wraith Essence');
