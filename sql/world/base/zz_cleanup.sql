b1986264ae4c939f44ab46ad38d95a43c0f7f156
郑佩茹 <zhengpeiru21@protonmail.com>
2023-09-03 10:27:37 -0600

    Fix several console warnings

--- sql/world/base/zz_cleanup.sql
+++ sql/world/base/zz_cleanup.sql
@@ -7,3 +7,6 @@ UPDATE `creature_template` SET `unit_class` = 2 WHERE `entry` IN (22742, 22791, 
                                                                  37245, 37256, 37443, 37472, 37341);
 UPDATE `creature_template` SET `unit_class` = 1 WHERE `entry` IN (22632, 32055, 31921, 37376);
 UPDATE `creature_template` SET `unit_class` = 8 WHERE `entry` IN (29273);
+
+/* Delete unused Creature Addon data */
+DELETE FROM `creature_addon` WHERE `guid` IN (133917, 133918, 133919, 133920, 133928);
