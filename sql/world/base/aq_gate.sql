803638a76777a4e4df8ebc570933f5c085c8943a
Dinkledork <118951051+Day36512@users.noreply.github.com>
2024-01-29 14:20:44 -0700

    Open Gates for players beyond Stage 5

---
+++ sql/world/base/aq_gate.sql
@@ -0,0 +1,3 @@
+UPDATE `acore_world`.`gameobject_template` SET `ScriptName`='aq_gate' WHERE  `entry`=176148;
+UPDATE `acore_world`.`gameobject_template` SET `ScriptName`='aq_gate' WHERE  `entry`=176147;
+UPDATE `acore_world`.`gameobject_template` SET `ScriptName`='aq_gate' WHERE  `entry`=176146;
