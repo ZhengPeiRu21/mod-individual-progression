8f0a9e1076834c99070e8da57cd17bb3984343c3
郑佩茹 <zhengpeiru21@protonmail.com>
2024-04-18 15:39:38 -0600

    Remove hard-coded database name

--- sql/world/base/aq_gate.sql
+++ sql/world/base/aq_gate.sql
@@ -1,3 +1,3 @@
-UPDATE `acore_world`.`gameobject_template` SET `ScriptName`='aq_gate' WHERE  `entry`=176148;
-UPDATE `acore_world`.`gameobject_template` SET `ScriptName`='aq_gate' WHERE  `entry`=176147;
-UPDATE `acore_world`.`gameobject_template` SET `ScriptName`='aq_gate' WHERE  `entry`=176146;
+UPDATE `gameobject_template` SET `ScriptName`='aq_gate' WHERE  `entry`=176148;
+UPDATE `gameobject_template` SET `ScriptName`='aq_gate' WHERE  `entry`=176147;
+UPDATE `gameobject_template` SET `ScriptName`='aq_gate' WHERE  `entry`=176146;
