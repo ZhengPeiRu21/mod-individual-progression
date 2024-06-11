05affcc1ed8cb2b0f5dbaa0e4fcb1fde1d06d5ad
Jelle Meeus <sogladev@gmail.com>
2024-06-11 21:04:49 +0200

    add scaling

--- sql/world/base/naxx40_creatures.sql
+++ sql/world/base/naxx40_creatures.sql
@@ -5717,3 +5717,20 @@ DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (351033, 351034)) AND (`sour
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (351033, 0, 0, 0, 76, 0, 100, 0, 0, 181202, 54, 1500, 0, 0, 41, 200, 10, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Diseased Maggot - On Distance 54y To GameObject - Despawn In 200 ms'),
 (351034, 0, 0, 0, 76, 0, 100, 0, 0, 181202, 54, 1500, 0, 0, 41, 200, 10, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Rotten Maggot - On Distance 54y To GameObject - Despawn In 200 ms');
+
+-- Scaling
+SET @DAMAGE_MULTIPLIER:= 0.69;
+SET @HEALTH_MULTIPLIER:= 1.00;
+SET @ARMOR_MULTIPLIER:=  1.00;
+
+UPDATE `creature_template`
+SET
+    `DamageModifier` = `DamageModifier` * @DAMAGE_MULTIPLIER,
+    `HealthModifier` = `HealthModifier` * @HEALTH_MULTIPLIER,
+    `ArmorModifier`  = `ArmorModifier`  * @ARMOR_MULTIPLIER
+WHERE `entry` IN (
+    SELECT `id1`
+    FROM `creature`
+    WHERE `map` = 533 AND `spawnMask` = 4
+);
+
