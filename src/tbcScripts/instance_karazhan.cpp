c24f0285d1b82a75ffcbf6b1065dc06683870a59
郑佩茹 <zhengpeiru21@protonmail.com>
2024-07-01 13:00:32 -0600

    Update Nightbane blackened urn script

--- src/tbcScripts/instance_karazhan.cpp
+++ src/tbcScripts/instance_karazhan.cpp
@@ -21,17 +21,6 @@ public:
     //right now that doesn't work because of how the urn works
     bool OnGossipHello(Player* player, GameObject* go) override
     {
-        float IntroWay[8][3] =
-        {
-            {-11053.37f, -1794.48f, 149.00f},
-            {-11141.07f, -1841.40f, 125.00f},
-            {-11187.28f, -1890.23f, 125.00f},
-            {-11189.20f, -1931.25f, 125.00f},
-            {-11153.76f, -1948.93f, 125.00f},
-            {-11128.73f, -1929.75f, 125.00f},
-            {-11140.00f, -1915.00f, 122.00f},
-            {-11163.00f, -1903.00f, 91.473f}
-        }; //TODO: move to table
 
         if (player->HasItemCount(ITEM_BLACKENED_URN, 1))
         {
@@ -39,8 +28,12 @@ public:
             {
                 if (pInstance->GetData(DATA_NIGHTBANE) != DONE && !go->FindNearestCreature(NPC_NIGHTBANE, 40.0f))
                     if (Creature* cr = ObjectAccessor::GetCreature(*player, pInstance->GetGuidData(DATA_NIGHTBANE)))
-                        cr->GetMotionMaster()->MovePoint(0, IntroWay[0][0], IntroWay[0][1], IntroWay[0][2]);
+                        cr->AI()->DoAction(0 /*ACTION_START_INTRO */);
             }
+        }
+        else
+        {
+            player->SendSystemMessage("You must possess a Blackened Urn in order to summon Nightbane!");
         }
         return false;
     }
