902c0adfa1cf7d4d58d1678b1ccfee9bc0cdb95e
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2024-08-22 05:43:13 +0200

    Update tbc_vendors.sql
    added script npc_ipp_tbc_pre_t4 to copy of Nakodu

--- sql/world/base/tbc_vendors.sql
+++ sql/world/base/tbc_vendors.sql
@@ -6,7 +6,7 @@ SET @Nakodu := 121655;
 /* Add copy of Nakodu to creature_template with script npc_ipp_tbc_pre_t4 */
 DELETE FROM `creature_template` WHERE `entry` = @Nakodu;
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) 
-VALUES (@Nakodu, 0, 0, 0, 0, 0, 'Nakodu', 'Lower City Quartermaster', NULL, 0, 70, 70, 1, 1818, 128, 1, 1, 1, 1, 20, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 33024, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 12340);
+VALUES (@Nakodu, 0, 0, 0, 0, 0, 'Nakodu', 'Lower City Quartermaster', NULL, 0, 70, 70, 1, 1818, 128, 1, 1, 1, 1, 20, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 33024, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 2, 'npc_ipp_tbc_pre_t4', 12340);
 
 /* Add copy of Nakodu to creature_template_addon */
 DELETE FROM `creature_template_addon` WHERE `entry` = @Nakodu;
