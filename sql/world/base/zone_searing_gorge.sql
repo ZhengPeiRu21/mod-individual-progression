500b4e54206ac81faa8b179af44ea94f8bf3633a
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2024-10-05 14:21:40 +0200

    link Maltorius with 2 sentries
    It was possible to single pull Maltorius.
    This solves it.

--- sql/world/base/zone_searing_gorge.sql
+++ sql/world/base/zone_searing_gorge.sql
@@ -13,3 +13,9 @@ UPDATE `creature` SET `id1` = 8504 WHERE `guid` = 5846;
 DELETE FROM `creature` WHERE `guid` = 608504;
 INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES 
 (608504, 8504, 0, 0, 0, 0, 0, 1, 1, 1, -6630.98, -1233.1, 209.809, 1.29509, 300, 0, 0, 2578, 0, 0, 0, 0, 0, '', NULL, 0, NULL);
+
+/* Group Maltorius together with the 2 sentries */
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES 
+(5845, 5845, 0, 0, 3, 0, 0),
+(5845, 5846, 0, 0, 3, 0, 0),
+(5845, 608504, 0, 0, 3, 0, 0);
