d889d51bd54f1d0147ba23ecea115df79c6a5a6f
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2024-10-07 22:46:39 +0200

    Update zone_bloodmyst_isle.sql
    "Cookie" McWeaksauce <Cooking Trainer & Supplies>
    
    This is a TBC npc, it's debatable if this one should be changed.
    It was decided to include the Draenei starting zones to the vanilla experience, but I think starting out here should not give an advantage so that's why I removed the ability for this npc to train expert and artisan cooking. Together with removing the spiced bread recipe, which is too convenient for a vanilla cooking recipe.

--- sql/world/base/zone_bloodmyst_isle.sql
+++ sql/world/base/zone_bloodmyst_isle.sql
@@ -1,3 +1,7 @@
  -- Anchorite Paetheus <First Aid Trainer> 
  DELETE FROM `npc_trainer` WHERE `ID`=17424; 
  INSERT INTO `npc_trainer` (`ID`, `SpellID`) VALUES (17424, -350000); 
+
+-- Cookie McWeaksauce <Cooking Trainer & Supplies>
+DELETE FROM `npc_trainer` WHERE `ID`=17246;
+INSERT INTO `npc_trainer` (`ID`, `SpellID`) VALUES (17246, -320000);
