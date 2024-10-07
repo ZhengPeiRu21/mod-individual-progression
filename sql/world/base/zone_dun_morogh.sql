e24804541eb27633a6462a95d540eedd4219591e
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2024-10-07 23:35:38 +0200

    Update zone_dun_morogh.sql
    - gave Thamner Pol <Physician> correct first aid recipes

--- sql/world/base/zone_dun_morogh.sql
+++ sql/world/base/zone_dun_morogh.sql
@@ -12,3 +12,6 @@ UPDATE `quest_template` SET `Flags`=8 WHERE `ID`=1599;
 -- Beginnings (Warlock)
 UPDATE `quest_template_addon` SET `SpecialFlags`=0 WHERE `ID`=1599;
 
+ -- Thamner Pol <Physician> 
+ DELETE FROM `npc_trainer` WHERE `ID`=2326;
+ INSERT INTO `npc_trainer` (`ID`, `SpellID`) VALUES (2326, -350000);
