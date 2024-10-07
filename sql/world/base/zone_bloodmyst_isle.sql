17f624b452e05e4ae0baefc3481a2e3714122a47
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2024-10-07 18:05:36 +0200

    Update zone_bloodmyst_isle.sql

--- sql/world/base/zone_bloodmyst_isle.sql
+++ sql/world/base/zone_bloodmyst_isle.sql
@@ -1,3 +1,3 @@
- -- Rawrk <First Aid Trainer> 
+ -- Anchorite Paetheus <First Aid Trainer> 
  DELETE FROM `npc_trainer` WHERE `ID`=17424; 
  INSERT INTO `npc_trainer` (`ID`, `SpellID`) VALUES (17424, -350000); 
