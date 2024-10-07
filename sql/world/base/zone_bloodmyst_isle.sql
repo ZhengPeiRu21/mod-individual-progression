9019ae584025643ba452c47247eaf73be61f29f7
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2024-10-07 23:01:28 +0200

    Update zone_bloodmyst_isle.sql
    Cookie McWeaksauce <Cooking Trainer & Supplies>
    
    I found out this npc was already changed.
    The spiced bread recipe is set to 200 skill requirement.

--- sql/world/base/zone_bloodmyst_isle.sql
+++ sql/world/base/zone_bloodmyst_isle.sql
@@ -1,7 +1,3 @@
  -- Anchorite Paetheus <First Aid Trainer> 
  DELETE FROM `npc_trainer` WHERE `ID`=17424; 
  INSERT INTO `npc_trainer` (`ID`, `SpellID`) VALUES (17424, -350000); 
-
--- Cookie McWeaksauce <Cooking Trainer & Supplies>
-DELETE FROM `npc_trainer` WHERE `ID`=17246;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`) VALUES (17246, -320000);
