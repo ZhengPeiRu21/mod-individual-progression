6d5c7e9f793208e44864aafba815511f8f679b72
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2024-10-07 23:33:49 +0200

    Update zone_ironforge.sql
    - gave Nissa Firestone <First Aid Trainer> correct first aid recipes

--- sql/world/base/zone_ironforge.sql
+++ sql/world/base/zone_ironforge.sql
@@ -1,3 +1,7 @@
 -- Summon Felsteed (Warlock)
 DELETE FROM `creature_queststarter` WHERE `id`=5172 AND `quest`=4487;
 INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES (5172, 4487);
+
+-- Nissa Firestone <First Aid Trainer> 
+DELETE FROM `npc_trainer` WHERE `ID`=5150; 
+INSERT INTO `npc_trainer` (`ID`, `SpellID`) VALUES (5150, -350000); 
