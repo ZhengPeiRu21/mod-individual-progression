998f11e5abe0922267e0784eccaaf3b337946ed3
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2024-10-07 23:21:45 +0200

    Update zone_shattrath.sql
    - gave Mildred Fletcher <Physician> correct first aid recipes

--- sql/world/base/zone_shattrath.sql
+++ sql/world/base/zone_shattrath.sql
@@ -2,3 +2,7 @@
 DELETE FROM `npc_vendor` WHERE `entry`=18756 AND `item`=38089 AND `ExtendedCost`=0;
 INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
 (18756, 0, 38089, 1, 1800, 0, 0);
+
+-- Mildred Fletcher <Physician>
+DELETE FROM `npc_trainer` WHERE `ID`=19184; 
+INSERT INTO `npc_trainer` (`ID`, `SpellID`) VALUES (19184, -350000); 
