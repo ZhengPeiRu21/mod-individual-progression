d80938b8f3e54d2dd08b3b1ffcd54f55557d8da1
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2024-10-07 18:07:49 +0200

    Update zone_eversong_woods.sql
    - gave Kanaria <First Aid Trainer> correct first aid recipes

--- sql/world/base/zone_eversong_woods.sql
+++ sql/world/base/zone_eversong_woods.sql
@@ -7,3 +7,7 @@ DELETE FROM `creature_queststarter` WHERE `id`=15283 AND `quest`=8344;
 INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES (15283, 8344);
 INSERT INTO creature_loot_template (Entry, Item, Reference, Chance, QuestRequired, LootMode, GroupId, MinCount, MaxCount, Comment) VALUES
 (15298, 20935, 0, 80, 1, 1, 0, 1, 1, 'Tainted Arcane Wraith - Tainted Wraith Essence');
+
+ -- Kanaria <First Aid Trainer> 
+ DELETE FROM `npc_trainer` WHERE `ID`=16272; 
+ INSERT INTO `npc_trainer` (`ID`, `SpellID`) VALUES (16272, -350000); 
