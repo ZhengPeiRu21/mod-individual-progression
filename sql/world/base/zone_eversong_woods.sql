0bc28903106bd364a3c257041dc2959dd1f591d7
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2024-10-08 21:46:40 +0200

    Update zone_eversong_woods.sql
    fix

--- sql/world/base/zone_eversong_woods.sql
+++ sql/world/base/zone_eversong_woods.sql
@@ -6,7 +6,7 @@ INSERT INTO `creature_questender` (`id`, `quest`) VALUES (15283, 8344);
 DELETE FROM `creature_queststarter` WHERE `id`=15283 AND `quest`=8344;
 INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES (15283, 8344);
 
-DELETE FROM `creature_loot_template` WHERE `Entry`= 15298;
+DELETE FROM `creature_loot_template` WHERE `Entry`= 15298 AND `Item`=20935;
 INSERT INTO `creature_loot_template` (Entry, Item, Reference, Chance, QuestRequired, LootMode, GroupId, MinCount, MaxCount, Comment) VALUES
 (15298, 20935, 0, 80, 1, 1, 0, 1, 1, 'Tainted Arcane Wraith - Tainted Wraith Essence');
 
