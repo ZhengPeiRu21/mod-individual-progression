5ba1d2bb3b41ed0d1f5af290805aab033deff4d1
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2024-10-19 22:49:47 +0200

    Update vanilla_quest_levels.sql - An Unholy Alliance
    send player to Varimathras in the Undercity

--- sql/world/base/vanilla_quest_levels.sql
+++ sql/world/base/vanilla_quest_levels.sql
@@ -483,6 +483,9 @@ UPDATE quest_template SET RequiredItemId1 = 16208, RequiredItemId2 = 0, Required
 /*  Earthen Arise  */
 UPDATE quest_template SET RequiredItemId2 = 0, RequiredItemCount2 = 0 WHERE ID=6481;
 
+/* An Unholy Alliance */
+UPDATE `quest_template` SET `LogDescription` = "Take the Small Scroll to Varimathras in the Undercity." WHERE `ID` = 6522;
+
 /*  King of the Foulweald  */
 UPDATE quest_template SET RequiredItemId2 = 0, RequiredItemCount2 = 0 WHERE ID=6621;
 
