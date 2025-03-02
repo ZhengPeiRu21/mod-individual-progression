79cdc8d4b60c2927ec7aa105e91d5384af06aee1
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-03-02 04:46:12 +0100

    Add visibility sql for war effort npcs (progression stage 4)

--- sql/world/base/optional_ipp_aware_aq_war_effort_npcs.sql
+++ sql/world/base/optional_ipp_aware_aq_war_effort_npcs.sql
@@ -1,9 +1,9 @@
--- This SQL query makes specific NPCs visible related to the AQ War Effort event
--- See: https://github.com/azerothcore/mod-war-effort
+-- Makes related AQ War Effort NPCs visible at progression stage 4
+-- See this GitHub repository for an AzerothCore AQ War Effort event module: https://github.com/azerothcore/mod-war-effort
 UPDATE `creature_template` SET `ScriptName` = 'npc_ipp_aq' WHERE `entry` IN (15383, 15431, 15432, 15434, 15437, 15445, 15446, 
                                                                              15448, 15450, 15451, 15452, 15453, 15455, 15456, 
-                                                                             15457, 15700, 15459, 15460, 15469, 15477, 15508, 
-                                                                             15512, 15515, 15522, 15525, 15528, 15529, 15532, 
-                                                                             15533, 15534, 15535, 21968, 15738, 15737, 15739, 
-                                                                             15736, 15731, 15733, 15735, 15734, 21969, 
+                                                                             15457, 15700, 15701, 15459, 15460, 15469, 15477, 
+                                                                             15508, 15512, 15515, 15522, 15525, 15528, 15529, 
+                                                                             15532, 15533, 15534, 15535, 21968, 15738, 15737, 
+                                                                             15739, 15736, 15731, 15733, 15735, 15734, 21969, 
                                                                              15810, 15813, 15742, 15741, 15740, 15758, 15818);
