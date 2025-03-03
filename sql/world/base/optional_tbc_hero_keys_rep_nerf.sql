52d97a9b88f8bd859360eca521df846cd5c5b9a4
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-03-03 20:05:32 +0100

    Add various optional sql

---
+++ sql/world/base/optional_tbc_hero_keys_rep_nerf.sql
@@ -0,0 +1,9 @@
+/* Optional change. Obtaining keys for heroic dungeons in The Burning Crusade is conditional on having corresponding reputation at Revered rank.
+   This reduces the reputation requirements to obtain keys to Honored and thus allows you to go to heroic dungeons more quickly. 
+   This allows players to catch up fairly advanced server progression more easily. */
+UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30622);
+UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30637);
+UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30633);
+UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30634);
+UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30623);
+UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30635);
--- sql/world/base/optional_vanilla_aware_war_effort_npcs.sql
+++ sql/world/base/optional_vanilla_aware_war_effort_npcs.sql
@@ -1,5 +1,5 @@
--- Makes related AQ War Effort NPCs visible at progression stage 4
--- See this GitHub repository for an AzerothCore AQ War Effort event module: https://github.com/azerothcore/mod-war-effort
+/* Makes related AQ War Effort NPCs visible at progression stage 4 (pre aq stage)
+See this GitHub repository for an AzerothCore AQ War Effort event module: https://github.com/azerothcore/mod-war-effort */
 UPDATE `creature_template` SET `ScriptName` = 'npc_ipp_aq' WHERE `entry` IN (15383, 15431, 15432, 15434, 15437, 15445, 15446, 
                                                                              15448, 15450, 15451, 15452, 15453, 15455, 15456, 
                                                                              15457, 15700, 15701, 15459, 15460, 15469, 15477, 
