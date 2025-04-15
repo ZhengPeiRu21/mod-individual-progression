6991c7ae4fcd80326c6887a1482e9269c28f2968
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-04-15 18:34:08 +0200

    LBRS fixes

--- sql/world/base/dungeon_blackrock_spire.sql
+++ sql/world/base/dungeon_blackrock_spire.sql
@@ -18,3 +18,12 @@ UPDATE `gameobject` SET `animprogress`=100, `state`=1 WHERE `id`=175532;
 
 -- Doodad_DarkIronBrazier06
 UPDATE `gameobject` SET `animprogress`=100, `state`=1 WHERE `id`=175533;
+
+
+/* important blackrock documents (item 12562) is currently a dropped item. it shouldn't be.
+this item can be found as a lootable scroll on the ground within LBRS. */
+DELETE FROM `creature_loot_template` WHERE `Item` = 12562;
+
+/* gemstones shouldn't have 100% drop rate. should be around 33%.
+exact rates unknown. wowhead's listed drop rates aren't correct. */
+UPDATE `creature_loot_template` SET `Chance` = 33 WHERE `Item` IN (12335, 12336, 12337);
