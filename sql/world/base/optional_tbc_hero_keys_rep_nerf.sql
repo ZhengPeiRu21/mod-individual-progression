63a698897d4dd14b9c1fbb154b8e45d83ef3df67
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-05-03 23:20:25 +0200

    update optional sql

--- sql/world/base/optional_tbc_hero_keys_rep_nerf.sql
+++ sql/world/base/optional_tbc_hero_keys_rep_nerf.sql
@@ -1,6 +1,8 @@
 /* Optional change. Obtaining keys for heroic dungeons in The Burning Crusade is conditional on having corresponding reputation at Revered rank.
    This reduces the reputation requirements to obtain keys to Honored and thus allows you to go to heroic dungeons more quickly. 
-   This allows players to catch up fairly advanced server progression more easily. */
+   This allows players to catch up fairly advanced server progression more easily.
+   This rep nerf was officially implemented in patch 2.3 (Zul'Aman raid content, 2008). 
+   Sql edit example: RequiredReputationRank = 6 for Revered, 5 for Honored, 4 for Friendly */
 UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30622);
 UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30637);
 UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30633);
