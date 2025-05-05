61133c086be1433af59aa6a52ce800149f9b82dc
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-05-05 13:22:36 +0200

    added comments
    added comments so people know what to edit.

--- sql/world/base/optional_tbc_heroic_dungeon_keys_nerf.sql
+++ sql/world/base/optional_tbc_heroic_dungeon_keys_nerf.sql
@@ -1,11 +1,11 @@
 /* Optional change. Obtaining keys for heroic dungeons in The Burning Crusade is conditional on having corresponding reputation at Revered rank.
    This reduces the reputation requirements to obtain keys to Honored and thus allows you to go to heroic dungeons more quickly. 
-   This allows players to catch up fairly advanced server progression more easily.
    This rep nerf was officially implemented in patch 2.3 (Zul'Aman raid content, 2008). 
    Sql edit example: RequiredReputationRank = 6 for Revered, 5 for Honored, 4 for Friendly */
-UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30622);
-UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30637);
-UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30633);
-UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30634);
-UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30623);
-UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` = 30635);
+
+UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE `entry` = 30622; -- Honor Hold, Flamewrought Key
+UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE `entry` = 30623; -- Cenarion Expedition, Reservoir Key
+UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE `entry` = 30633; -- Lower City, Auchenai Key
+UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE `entry` = 30634; -- The Sha'tar, Warpforged Key
+UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE `entry` = 30635; -- Keepers of Time, Key of Time
+UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE `entry` = 30637; -- Thrallmar, Flamewrought Key
