205a3693dc1441b5f1f16811842321c3abbe4693
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-05-07 17:16:37 +0200

    wrong column checked
    wrong column checked for delete before insert.
    gave error if the sql was run twice.

--- sql/world/base/spell_restorations.sql
+++ sql/world/base/spell_restorations.sql
@@ -79,9 +79,9 @@ UPDATE `npc_trainer` SET `ReqLevel` = 20 WHERE `SpellID` = 2645;
 UPDATE `npc_trainer` SET `ReqLevel` = 20 WHERE `SpellID` = 5118;
 
 /* Highborne undead in Winterspring missing smart AI */
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 7523 AND `event_param1` = 14868;
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 7524 AND `event_param1` = 13860;
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 10684 AND `event_param1` = 13860;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 7523 AND `action_param1` = 14868;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 7524 AND `action_param1` = 13860;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 10684 AND `action_param1` = 13860;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 /* Suffering Highborne (7523) */
 (7523, 0, 4, 0, 0, 0, 100, 0, 3000, 6000, 13000, 19000, 0, 0, 11, 14868, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'Suffering Highborne - In Combat - Cast Curse of Agony'),
