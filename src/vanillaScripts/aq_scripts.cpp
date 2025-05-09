8555ab941925151dff46526a37720b74aed6f231
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-05-09 16:09:29 +0200

    Update aq_scripts.cpp

--- src/vanillaScripts/aq_scripts.cpp
+++ src/vanillaScripts/aq_scripts.cpp
@@ -224,11 +224,9 @@ public:
             {
                 return false; 
             }
-
             // (RequirePreAQQuests = 1) - AQ gate closed after Nefarian kill. War effort starts. AQ War effort + AQ Quest line needs to be done to open the gate. 
             // (RequirePreAQQuests = 0) - AQ gate closed after Nefarian kill. War effort starts. AQ War effort needs to be done to open the gate. 
-            return sIndividualProgression->hasPassedProgression(target, PROGRESSION_PRE_AQ);
-            
+            return sIndividualProgression->isBeforeProgression(target, PROGRESSION_AQ);            
         }
     };
 
