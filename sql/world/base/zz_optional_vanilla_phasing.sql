644f5c34050dc55216a94f56efa285a4bea87627
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-05-11 14:46:23 +0200

    Update and rename optional_vanilla_phasing.sql to zz_optional_vanilla_phasing.sql
    consistency
    
    commented out the phasing of the flight masters at Ratchet and Marshal's Refuge
    Most player will expect these npcs to be there.

--- sql/world/base/zz_optional_vanilla_phasing.sql
+++ sql/world/base/zz_optional_vanilla_phasing.sql
@@ -1,4 +1,5 @@
-/*  This will phase Vanilla/TBC npcs & objects placed in vanilla areas until they were originally added to the game.
+/*  
+    This will phase Vanilla/TBC npcs & objects placed in vanilla areas until they were originally added to the game.
     This includes quest givers and flight paths. 
 */
 
@@ -15,17 +16,20 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_ipp_tbc' WHERE `entry` IN (
 17194, -- Anchorite Delan, Darkshire
 17218, -- Huraan, Southshore
 17223, -- Ambassador Rualeth, Aerie Peak
-22931, -- Gorrim, Emerald Sanctuary
-24366, -- Nizzle, Rebel Camp
+22931, -- Gorrim, Emerald Sanctuary, Flight Master
+24366, -- Nizzle, Rebel Camp, Flight Master
 27705, -- Lorrin Foxfire, Stonard
-37888, -- Frax Bucketdrop, Thondroril River
-37915 -- Timothy Cunningham, The Bulwark
+37888, -- Frax Bucketdrop, Thondroril River, Flight Master
+37915 -- Timothy Cunningham, The Bulwark, Flight Master
 );
 
+/* the flight masters at Ratchet and Marshal's Refuge were originally added in patch 1.11
 UPDATE `creature_template` SET `ScriptName` = 'npc_ipp_naxx40' WHERE `entry` IN (
-10583, -- Gryfe, Marshal's Refuge
-16227 -- Bragok, Ratchet
-);
+10583, -- Gryfe, Marshal's Refuge, Flight Master
+16227 -- Bragok, Ratchet, Flight Master); 
+*/
 
 -- remove interactions between Cersei, Lorrin and the orcs in Stonard 
 UPDATE `creature_template` SET `AIName` = '' WHERE `entry` IN (12807, 17109, 27705);
+
+-- to do: mail boxes
