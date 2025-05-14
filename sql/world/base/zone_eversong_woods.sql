d3ec884a29616414a1113d9c471c7783c380cfdb
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-05-14 14:25:10 +0200

    Update zone_eversong_woods.sql
    code moved to professions.sql

--- sql/world/base/zone_eversong_woods.sql
+++ sql/world/base/zone_eversong_woods.sql
@@ -9,7 +9,3 @@ INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES (15283, 8344);
 DELETE FROM `creature_loot_template` WHERE `Entry`= 15298 AND `Item`=20935;
 INSERT INTO `creature_loot_template` (Entry, Item, Reference, Chance, QuestRequired, LootMode, GroupId, MinCount, MaxCount, Comment) VALUES
 (15298, 20935, 0, 80, 1, 1, 0, 1, 1, 'Tainted Arcane Wraith - Tainted Wraith Essence');
-
- -- Kanaria <First Aid Trainer> 
- DELETE FROM `npc_trainer` WHERE `ID`=16272; 
- INSERT INTO `npc_trainer` (`ID`, `SpellID`) VALUES (16272, -350000); 
