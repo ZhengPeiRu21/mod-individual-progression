854bf9ec3f5105ff3f018ae4bb2f79cf93b0ce11
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-06-08 04:07:20 +0200

    Delete sql/world/base/tbc_vendors.sql
    code moved to zone_shattrath

--- sql/world/base/tbc_vendors.sql
+++
@@ -1,9 +0,0 @@
-/* Hide certain vendor items until the player has reached the progression tier for them */
-
-DELETE FROM `conditions` WHERE `SourceEntry` IN (); 
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, 
-`ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
--- 
-(23, 21655, 34200, 0, 0, 8, 0, 66011, 0, 0, 0, 0, 0, '', 'Nakodu will not sell Pattern: Quiver of a Thousand Feathers until the player has completed PROGRESSION_TBC_TIER_3'),
-(23, 17585, 34201, 0, 0, 8, 0, 66011, 0, 0, 0, 0, 0, '', 'Urgronn will not sell Pattern: Netherscale Ammo Pouch until the player has completed PROGRESSION_TBC_TIER_3'),
-(23, 17657, 34201, 0, 0, 8, 0, 66011, 0, 0, 0, 0, 0, '', 'Ulrike will not sell Pattern: Netherscale Ammo Pouch until the player has completed PROGRESSION_TBC_TIER_3');
