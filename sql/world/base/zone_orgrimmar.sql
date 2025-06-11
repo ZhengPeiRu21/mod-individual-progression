5aed869b828d52b097862e7fca6141e296956606
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-06-11 06:44:07 +0200

    Update zone_orgrimmar.sql

--- sql/world/base/zone_orgrimmar.sql
+++ sql/world/base/zone_orgrimmar.sql
@@ -657,7 +657,7 @@ INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `ExtendedCost
 
 
 /* Hide certain vendor items until the player has reached the progression tier for them */
-DELETE FROM `conditions` WHERE `SourceGroup` IN (12792, @TH_Classic, @TH_TBC);
+DELETE FROM `conditions` WHERE `SourceGroup` IN (@LP_Classic, @LP_TBC, @TH_Classic, @TH_TBC);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, 
 `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
 -- 
