18af843876653f56643c8a5fb9541492a7ea65ac
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-06-14 14:12:45 +0200

    Update quest_missing_diplomat.sql

--- sql/world/base/quest_missing_diplomat.sql
+++ sql/world/base/quest_missing_diplomat.sql
@@ -53,11 +53,11 @@ INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionTex
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 2465 AND `ConditionValue1` = 11222;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, 
 `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
-(15, 2465, 0, 0, 0, 28, 0, 11222, 0, 0, 0, 0, 0, '', '');
+(15, 2465, 1, 0, 0, 28, 0, 11222, 0, 0, 0, 0, 0, '', '');
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 4968 AND `id` IN (9, 10);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
-(4968, 0, 9, 1, 62, 0, 100, 512, 2465, 0, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(4968, 0, 9, 1, 62, 0, 100, 512, 2465, 1, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, ''),
 (4968, 0, 10, 0, 61, 0, 100, 512, 0, 0, 0, 0, 0, 0, 11, 42710, 2, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '');
 
 
