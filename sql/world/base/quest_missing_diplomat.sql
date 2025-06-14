ddebc73492c05fdba5b94aafb2ce34a12807a499
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-06-14 13:54:54 +0200

    add teleport to Stormwind

--- sql/world/base/quest_missing_diplomat.sql
+++ sql/world/base/quest_missing_diplomat.sql
@@ -40,6 +40,26 @@ UPDATE `quest_template` SET
     `RewardChoiceItemQuantity3` = 0,
 WHERE `ID` = 11142;
 
+UPDATE `gossip_menu_option` SET `OptionText` = 'Can you send me to Theramore? I have an urgent message for Lady Jaina from Highlord Bolvar.' WHERE `MenuID` = 8846;
+UPDATE `broadcast_text`     SET   `MaleText` = 'Can you send me to Theramore? I have an urgent message for Lady Jaina from Highlord Bolvar.', 
+                                `FemaleText` = 'Can you send me to Theramore? I have an urgent message for Lady Jaina from Highlord Bolvar.' WHERE `ID` = 22524;
+
+
+DELETE FROM `gossip_menu_option` WHERE `MenuID` = 2465 AND `OptionID` = 1;
+INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, 
+`OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES 
+(2465, 1, 0, 'I\'m ready to travel to Stormwind.', 22522, 1, 1, 0, 0, 0, 0, '', 0, 0);
+
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 2465 AND `ConditionValue1` = 11222;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, 
+`ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
+(15, 2465, 0, 0, 0, 28, 0, 11222, 0, 0, 0, 0, 0, '', '');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 4968 AND `id` IN (9, 10);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(4968, 0, 9, 1, 62, 0, 100, 512, 2465, 0, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(4968, 0, 10, 0, 61, 0, 100, 512, 0, 0, 0, 0, 0, 0, 11, 42710, 2, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '');
+
 
 -- Lady Prestor, Bolvar and King Wrynn updates
 
