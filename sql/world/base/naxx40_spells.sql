8ee7a1a7ee8dbb4e615329228932a69a03ce3b9f
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-09 18:33:45 +0200

    Update naxx40_spells.sql

--- sql/world/base/naxx40_spells.sql
+++ sql/world/base/naxx40_spells.sql
@@ -63,7 +63,7 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (28522, 'spel
 DELETE FROM `spell_script_names` WHERE `spell_id` = 28531;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (28531, 'spell_sapphiron_frost_aura_40');
 
--- 60960 War Stomp: Reduce damage
+-- 60960 War Stomp: Increase damage 500 to ~1000
 DELETE FROM `spell_script_names` WHERE `spell_id` = 60960;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (60960, 'spell_patchwork_golem_war_stomp_40');
 
