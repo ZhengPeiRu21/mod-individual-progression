e6463310ba362a774ed99677e913b4599595959e
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-07-16 14:09:14 +0200

    comment POI optional sql

--- sql/world/base/zz_optional_remove_quest_markers.sql
+++ sql/world/base/zz_optional_remove_quest_markers.sql
@@ -3,7 +3,7 @@
    by default. Because the opinion is so mixed, I have changed it to be enabled by default, as this is the most accurate to pre-3.2 WoW and the quests
    were designed to be played without markers. But please feel free to comment out or delete this file if you prefer to have them! */
 
-DELETE FROM `gameobject_questitem` WHERE `GameObjectEntry` BETWEEN 32 AND 201937;
-DELETE FROM `quest_poi` WHERE `QuestID` BETWEEN 1 AND 25495;
-DELETE FROM `quest_poi_points` WHERE `QuestID` BETWEEN 1 AND 25495;
-DELETE FROM `creature_questitem` WHERE `CreatureEntry` BETWEEN 3 AND 39819;
+-- DELETE FROM `gameobject_questitem` WHERE `GameObjectEntry` BETWEEN 32 AND 201937;
+-- DELETE FROM `quest_poi` WHERE `QuestID` BETWEEN 1 AND 25495;
+-- DELETE FROM `quest_poi_points` WHERE `QuestID` BETWEEN 1 AND 25495;
+-- DELETE FROM `creature_questitem` WHERE `CreatureEntry` BETWEEN 3 AND 39819;
