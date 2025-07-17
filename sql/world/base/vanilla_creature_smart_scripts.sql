7c99b06733eb6c61f8cf3f3ddf1f3e6a8a4a0faa
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-17 20:40:50 +0200

    Update vanilla_creature_smart_scripts.sql

--- sql/world/base/vanilla_creature_smart_scripts.sql
+++ sql/world/base/vanilla_creature_smart_scripts.sql
@@ -4,10 +4,12 @@
 */
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN 
-(232, 1892, 1893, 1896, 1934, 1935, 2006, 2007, 2010, 2160, 2956, 2979, 3058, 3068, 3244, 3245, 3246, 3279, 3284, 3285, 3634, 5785, 5807, 7523, 7524, 10684, 10737);
+(232, 1892, 1893, 1896, 1934, 1935, 2006, 2007, 2010, 2160, 2956, 2979, 3058, 3068, 3244, 3245, 3246, 3279, 3284, 3285, 3398, 3634, 3932, 3999, 
+ 4022, 5785, 5807, 7523, 7524, 10684, 10737, 10882);
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN 
-(232, 1892, 1893, 1896, 1934, 1935, 2006, 2007, 2010, 2160, 2956, 2979, 3058, 3068, 3244, 3245, 3246, 3279, 3284, 3285, 3634, 5785, 5807, 7523, 7524, 10684, 10737);
+(232, 1892, 1893, 1896, 1934, 1935, 2006, 2007, 2010, 2160, 2956, 2979, 3058, 3068, 3244, 3245, 3246, 3279, 3284, 3285, 3398, 3634, 3932, 3999, 
+ 4022, 5785, 5807, 7523, 7524, 10684, 10737, 10882);
 
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
 `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
@@ -15,44 +17,56 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 
 -- Hillsbrad Foothills
-(232,  0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Farmer Ray - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(232,  0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Farmer Ray - Between 0-15% Health - Flee For Assist (No Repeat)'),
 
 -- Silverpine Forest
-(1892, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Moonrage Watcher - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(1893, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Moonrage Sentry - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(1896, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Moonrage Elder - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(1892, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Moonrage Watcher - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(1893, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Moonrage Sentry - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(1896, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Moonrage Elder - Between 0-15% Health - Flee For Assist (No Repeat)'),
 
 -- Tirisfall Glades
-(1934, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Tirisfal Farmer - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(1935, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Tirisfal Farmhand - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(1934, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Tirisfal Farmer - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(1935, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Tirisfal Farmhand - Between 0-15% Health - Flee For Assist (No Repeat)'),
 
 -- Teldrassil
-(2006, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Gnarlpine Ursa - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(2007, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Gnarlpine Gardener - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(2010, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Gnarlpine Defender - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(2006, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Gnarlpine Ursa - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(2007, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Gnarlpine Gardener - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(2010, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Gnarlpine Defender - Between 0-15% Health - Flee For Assist (No Repeat)'),
 
 -- Darkshore
-(2160, 0, 0, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 12544, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,       'Gravelflint Geomancer - On Spawn - Cast Frost Armor'),
-(2160, 0, 1, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Gravelflint Geomancer - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(2160, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 12544, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                         'Gravelflint Geomancer - On Reset - Cast Frost Armor'),
+(2160, 0, 1, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Gravelflint Geomancer - Between 0-15% Health - Flee For Assist (No Repeat)'),
 
+-- Ashenvale
+(3932, 0, 0, 0, 2, 0, 100, 1, 0, 30, 0, 0, 0, 0, 11, 8599, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                          'Bloodtooth Guard - Between 0-30% Health - Cast Enrage (No Repeat)'),
+(3932, 0, 1, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Bloodtooth Guard - Between 0-15% Health - Flee For Assist (No Repeat)'),
+
+-- Stonetalon Mountains
+(3999, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Windshear Digger - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(4022, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Bloodfury Harpy - Between 0-15% Health - Flee For Assist (No Repeat)'),
+    
  -- Mulgore   
-(2956, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Greater Plainstrider - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(2979, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Venture Co. Supervisor - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(3058, 0, 0, 0, 0, 0, 100, 0, 11000, 15000, 0, 0, 0, 0, 11, 6730, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arra chea - In Combat - Cast Head Butt'),
-(3068, 0, 0, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 3583, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,        'Mazzranache - On Spawn - Cast Deadly Poison'),
-(3068, 0, 1, 0, 4, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 6268, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,         'Mazzranache - On Aggro - Cast Rushing Charge'),
-(5785, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Sister Hatelash - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(5807, 0, 0, 0, 0, 0, 100, 0, 8000, 12000, 0, 0, 0, 0, 11, 12166, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'The Rake - In Combat - Cast Muscle Tear'),
+(2956, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Greater Plainstrider - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(2979, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Venture Co. Supervisor - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(3058, 0, 0, 0, 0, 0, 100, 0, 11000, 15000, 0, 0, 0, 0, 11, 6730, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,                   'Arra chea - In Combat - Cast Head Butt'),
+(3068, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 3583, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                          'Mazzranache - On Reset - Cast Deadly Poison'),
+(3068, 0, 1, 0, 4, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 6268, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                           'Mazzranache - On Aggro - Cast Rushing Charge'),
+(5785, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Sister Hatelash - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(5807, 0, 0, 0, 0, 0, 100, 0, 8000, 12000, 0, 0, 0, 0, 11, 12166, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,                   'The Rake - In Combat - Cast Muscle Tear'),
     
 -- The Barrens
-(3244, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Greater Plainstrider - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(3245, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Ornery Plainstrider - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(3246, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Fleeting Plainstrider - Between 0-15% Health - Flee For Assist (No Repeat)');
-(3279, 0, 0, 0, 4, 0, 20, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,              'Witchwing Ambusher - On Aggro - Say Line 0'),
-(3279, 0, 1, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Witchwing Ambusher - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(3284, 0, 0, 0, 2, 0, 100, 1, 0, 20, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Venture Co. Drudger - Between 0-20% Health - Flee For Assist (No Repeat)'),
-(3285, 0, 0, 0, 2, 0, 100, 1, 0, 20, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,           'Venture Co. Peon - Between 0-20% Health - Flee For Assist (No Repeat)'),
-(3634, 0, 0, 0, 1, 0, 100, 1, 1000, 1000, 0, 0, 0, 0, 11, 22766, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,  'Deviate Stalker - Out of Combat - Cast Sneak');
+(3244, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Greater Plainstrider - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(3245, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Ornery Plainstrider - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(3246, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Fleeting Plainstrider - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(3279, 0, 0, 0, 4, 0, 20, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,                                'Witchwing Ambusher - On Aggro - Say Line 0'),
+(3279, 0, 1, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Witchwing Ambusher - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(3284, 0, 0, 0, 2, 0, 100, 1, 0, 20, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Venture Co. Drudger - Between 0-20% Health - Flee For Assist (No Repeat)'),
+(3285, 0, 0, 0, 2, 0, 100, 1, 0, 20, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                             'Venture Co. Peon - Between 0-20% Health - Flee For Assist (No Repeat)'),
+(3398, 0, 0, 0, 0, 0, 100, 0, 5000, 8000, 120000, 130000, 0, 0, 11, 3583, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,          'Gesharahan - In Combat - Cast Deadly Poison'),
+(3634, 0, 0, 0, 1, 0, 100, 1, 1000, 1000, 0, 0, 0, 0, 11, 22766, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                    'Deviate Stalker - Out of Combat - Cast Sneak'),
+
+-- Thousand Needles
+(10882, 0, 0, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 17213, 0, 0, 0, 0, 0, 5, 30, 0, 0, 0, 0, 0, 0, 0,                       'Arikara - On Spawn - Cast Curse of Vengeance'),
     
 -- Winterspring
 (7523, 0, 2, 0, 38, 0, 100, 512, 1, 1, 0, 0, 0, 0, 2, 124, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                          'Suffering Highborne - On Data Set 1 1 - Set Faction 124'),
