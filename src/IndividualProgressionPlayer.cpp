17a8bf1ec4fedcfcc7527a3ab7b81fb153e3ea12
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-20 05:14:35 +0200

    Update IndividualProgressionPlayer.cpp

--- src/IndividualProgressionPlayer.cpp
+++ src/IndividualProgressionPlayer.cpp
@@ -1019,37 +1019,6 @@ public:
         }
     }
 
-    void OnPlayerQueueRandomDungeon(Player* player, uint32& rDungeonId) override
-    {
-        // List of exceptions for seasonal event dungeons
-        std::set<uint32> seasonalEventDungeons = { 285, 286, 287, 288 };
-        if (seasonalEventDungeons.find(rDungeonId) != seasonalEventDungeons.end())
-        {
-            return;
-        }
-
-        // Check if RDF is disabled in the context of Individual Progression
-        if (sConfigMgr->GetOption<bool>("IndividualProgression.DisableRDF", false))
-        {
-            ChatHandler(player->GetSession()).SendNotification("The Random Dungeon feature is currently disabled by the Individual Progression module.");
-            rDungeonId = 1000; // Set dungeon ID to an invalid value to cancel the queuing
-            return;
-        }
-
-        if (!sIndividualProgression->hasPassedProgression(player, PROGRESSION_PRE_TBC) || (!sIndividualProgression->hasPassedProgression(player, PROGRESSION_PRE_TBC) && (player->GetLevel() <= IP_LEVEL_VANILLA)))
-        {
-            rDungeonId = RDF_CLASSIC;
-        }
-        else if ((rDungeonId == RDF_WRATH_OF_THE_LICH_KING && !sIndividualProgression->hasPassedProgression(player, PROGRESSION_TBC_TIER_5)) || (!sIndividualProgression->hasPassedProgression(player, PROGRESSION_TBC_TIER_5) && (player->GetLevel() <= IP_LEVEL_TBC)))
-        {
-            rDungeonId = RDF_THE_BURNING_CRUSADE;
-        }
-        else if ((rDungeonId == RDF_WRATH_OF_THE_LICH_KING_HEROIC && !sIndividualProgression->hasPassedProgression(player, PROGRESSION_TBC_TIER_5)) || (!sIndividualProgression->hasPassedProgression(player, PROGRESSION_TBC_TIER_5) && (player->GetLevel() <= IP_LEVEL_TBC)))
-        {
-            rDungeonId = RDF_THE_BURNING_CRUSADE_HEROIC;
-        }
-    }
-
     bool OnPlayerCanEquipItem(Player* player, uint8 /*slot*/, uint16& /*dest*/, Item* pItem, bool /*swap*/, bool /*not_loading*/) override
     {
         if (sIndividualProgression->pvpGearRequirements)
