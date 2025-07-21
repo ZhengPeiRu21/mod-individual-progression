7f84daad47ef007d5c99d39c6a2a511d6588d901
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-21 23:15:59 +0200

    Update zone_barrens.sql

--- sql/world/base/zone_barrens.sql
+++ sql/world/base/zone_barrens.sql
@@ -139,7 +139,7 @@ INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Lan
 -- smart scripts
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (3244, 3245, 3246, 3279, 3398, 3634);
 
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (3244, 3245, 3246, 3279, 3398, 3634);
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` IN (3244, 3245, 3246, 3279, 3398, 3634);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
 `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
 `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
