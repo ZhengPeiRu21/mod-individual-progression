7b069b8720a9add8e45276d5518a31b604a9cb3a
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-21 20:32:27 +0200

    Update zone_tanaris.sql
    add smart script for Living Storm

--- sql/world/base/zone_tanaris.sql
+++ sql/world/base/zone_tanaris.sql
@@ -11,8 +11,8 @@ UPDATE `creature` SET `position_x` = -8175.67, `position_y` = -4718.28, `positio
 UPDATE `creature_loot_template` SET `Chance` = 10 WHERE `Item` = 8428;
 
 -- smart scripts
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (5452, 5465);
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (5452, 5465);
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (5452, 5465, 9397);
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (5452, 5465, 9397);
 
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
 `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
@@ -22,4 +22,5 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (5452, 0, 0, 0, 2, 0, 100, 1, 0, 30, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,      'Hazzali Worker - Between 0-30% Health - Flee For Assist (No Repeat)'),
 (5452, 0, 1, 0, 6, 0, 100, 513, 0, 0, 0, 0, 0, 0, 11, 11023, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Hazzali Worker - On Just Died - Cast Summon Hazzali Parasites'),
 --
-(5465, 0, 0, 0, 2, 0, 100, 0, 0, 30, 0, 0, 0, 0, 11, 8599, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,   'Land Rager - Between 0-30% Health - Cast Enrage');
+(5465, 0, 0, 0, 2, 0, 100, 0, 0, 30, 0, 0, 0, 0, 11, 8599, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,   'Land Rager - Between 0-30% Health - Cast Enrage'),
+(9397, 0, 0, 0, 4, 0, 100, 1, 0, 0, 0, 0, 0, 0, 11, 12550, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,  'Living Storm - On Aggro - Cast Lightning Shield (No Repeat)');
