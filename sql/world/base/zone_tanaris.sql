30c39159c38a73f31da0b4743620bb8d9d4fee08
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-21 19:37:44 +0200

    Update zone_tanaris.sql
    add smart script for Hazzali Worker

--- sql/world/base/zone_tanaris.sql
+++ sql/world/base/zone_tanaris.sql
@@ -9,3 +9,15 @@ UPDATE `creature` SET `position_x` = -8175.67, `position_y` = -4718.28, `positio
 
 -- update Laden Dew Gland drop rate, was 100%
 UPDATE `creature_loot_template` SET `Chance` = 10 WHERE `Item` = 8428;
+
+-- smart scripts
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (5452);
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (5452);
+
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
+`action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
+`target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+--
+(5452, 0, 0, 0, 2, 0, 100, 1, 0, 30, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,      'Hazzali Worker - Between 0-30% Health - Flee For Assist (No Repeat)'),
+(5452, 0, 1, 0, 6, 0, 100, 513, 0, 0, 0, 0, 0, 0, 11, 11023, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Hazzali Worker - On Just Died - Cast Summon Hazzali Parasites');
