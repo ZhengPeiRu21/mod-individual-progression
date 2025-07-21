64d7f6b684d209c755047043a49432ebfd97fbd0
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-21 05:51:25 +0200

    Update zone_stranglethorn.sql
    add smart script for Mosh Ogg Butcher

--- sql/world/base/zone_stranglethorn.sql
+++ sql/world/base/zone_stranglethorn.sql
@@ -8,13 +8,16 @@ UPDATE `creature_loot_template` SET `Chance` = 4 WHERE `Item` = 3862;
 UPDATE `creature_loot_template` SET `Chance` = 25 WHERE `Item` = 3863;
 
 
-DELETE FROM `creature_text` WHERE `CreatureID` = 675;
+DELETE FROM `creature_text` WHERE `CreatureID` IN (675, 723);
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES 
-(675, 0, 0, 'Work you dogs! Put your back into it!',  12, 0, 100, 0, 0, 0, 306, 0, 'Venture Co. Foreman');
+(675, 0, 0, 'Work you dogs! Put your back into it!',             12, 0, 100, 0, 0, 0, 306,  0, 'Venture Co. Foreman'),
+(723, 0, 0, 'I\'ll crush you!',                                  12, 0, 100, 0, 0, 0, 1925, 0, 'Mosh Ogg Butcher'),
+(723, 0, 1, 'Me smash! You die!',                                12, 0, 100, 0, 0, 0, 1926, 0, 'Mosh Ogg Butcher'),
+(723, 0, 2, 'Raaaaaaaaaaaaaaaaaaaaaaaaaaaaaar!!! Me smash $R!',  12, 0, 100, 0, 0, 0, 1927, 0, 'Mosh Ogg Butcher');
 
 
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (675);
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (675);
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (675, 723);
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (675, 723);
 
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
 `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
@@ -23,4 +26,5 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 --
 (675, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 8258, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,              'Venture Co. Foreman - On Reset - Cast Devotion Aura'),
 (675, 0, 1, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                 'Venture Co. Foreman - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(675, 0, 2, 0, 1, 0, 100, 0, 60000, 120000, 60000, 120000, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Venture Co. Foreman - Out of Combat - Say Line 0');
+(675, 0, 2, 0, 1, 0, 100, 0, 60000, 120000, 60000, 120000, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Venture Co. Foreman - Out of Combat - Say Line 0'),
+(723, 0, 0, 0, 4, 0, 80, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,                    'Mosh Ogg Butcher - On Aggro - Say Line 0');
