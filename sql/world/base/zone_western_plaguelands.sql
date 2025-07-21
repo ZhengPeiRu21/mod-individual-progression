0d2164a0600dbebfcfbc7e5c0dd1a5d53ea8bdd4
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-21 23:21:27 +0200

    Update zone_western_plaguelands.sql

--- sql/world/base/zone_western_plaguelands.sql
+++ sql/world/base/zone_western_plaguelands.sql
@@ -1,6 +1,5 @@
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (1845, 1846);
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (1845, 1846);
-
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` IN (1845, 1846);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
 `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
 `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
