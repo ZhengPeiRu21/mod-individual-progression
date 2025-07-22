ae118de697abeaa08abc3d48f575e5ce306012e1
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-22 18:59:45 +0200

    Update zone_stormwind.sql
    add smart script for Onyxia's Elite Guard

--- sql/world/base/zone_stormwind.sql
+++ sql/world/base/zone_stormwind.sql
@@ -502,6 +502,16 @@ UPDATE `quest_template` SET `LogTitle` = 'Chillwind Point' WHERE `ID` = 8415;
 -- Devourer of Souls (Warlock)
 UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE `ID` = 1716;
 
+-- smart scripts
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (12739);
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` IN (12739);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
+`action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
+`target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+--
+(12739, 0, 0, 1, 2, 0, 100, 0, 0, 30, 0, 0, 0, 0, 11, 8599, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Onyxia\'s Elite Guard - Between 0-30% Health - Cast Enrage'),
+(12739, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,     'Onyxia\'s Elite Guard - On Enrage - Say Line 0');
 
 -- this should probably go to vanilla_vendors
 -- Remove non-Vanilla food items from Stormwind vendors
