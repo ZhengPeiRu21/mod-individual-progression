03487bfa858aff165f5bb1c3f304389075d92567
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-22 19:33:48 +0200

    Update zone_winterspring.sql
    add smart script for Winterfall Ambusher

--- sql/world/base/zone_winterspring.sql
+++ sql/world/base/zone_winterspring.sql
@@ -1,5 +1,5 @@
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (7451, 7523, 7524, 10200, 10684, 10737);
-DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` IN (7451, 7523, 7524, 10200, 10684, 10737);
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (7451, 7523, 7524, 10200, 10684, 10737, 14372);
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` IN (7451, 7523, 7524, 10200, 10684, 10737, 14372);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
 `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
 `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
@@ -16,9 +16,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (10684, 0, 0, 0, 20, 0, 100, 0, 5248, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                'Remorseful Highborne - On Quest Tormented by the Past Finished - Say Line 0'),
 (10684, 0, 1, 0, 0, 0, 100, 0, 2200, 3400, 9800, 12700, 0, 0, 11, 13860, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,  'Remorseful Highborne - In Combat - Cast Mind Blast'),
 --
-(10200, 0, 0, 0, 2, 0, 100, 0, 0, 30, 0, 0, 0, 0, 11, 8599, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                         'Rak shiri - Between 0-30% Health - Cast Enrage'),
-(10200, 0, 1, 0, 2, 0, 100, 0, 0, 60, 15000, 21000, 0, 0, 11, 7399, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                 'Rak shiri - Between 0-60% Health - Cast Terrify'),
+(10200, 0, 0, 0, 2, 0, 100, 0, 0, 30, 0, 0, 0, 0, 11, 8599, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,               'Rak shiri - Between 0-30% Health - Cast Enrage'),
+(10200, 0, 1, 0, 2, 0, 100, 0, 0, 60, 15000, 21000, 0, 0, 11, 7399, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,       'Rak shiri - Between 0-60% Health - Cast Terrify'),
+--
 (10737, 0, 0, 0, 0, 0, 100, 0, 5000, 5000, 5000, 10000, 0, 0, 11, 14100, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,            'Shy-Rotam - In Combat - Cast Terrifying Roar '),
 (10737, 0, 1, 0, 0, 0, 100, 0, 7000, 9000, 22000, 27000, 0, 0, 11, 13443, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,           'Shy-Rotam - In Combat - Cast Rend'),
 (10737, 0, 2, 0, 0, 0, 100, 0, 2000, 3000, 15000, 20000, 0, 0, 11, 3604, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,            'Shy-Rotam - In Combat - Cast Tendon Rip'),
-(10737, 0, 3, 0, 2, 0, 100, 1, 0, 50, 0, 0, 0, 0, 12, 10741, 1, 180000, 0, 0, 0, 8, 0, 0, 0, 0, 8074.84, -3840, 690.061, 4.6, 'Shy-Rotam - Between 0-50% health - Summon Creature Sian-Rotam (No Repeat)');
+(10737, 0, 3, 0, 2, 0, 100, 1, 0, 50, 0, 0, 0, 0, 12, 10741, 1, 180000, 0, 0, 0, 8, 0, 0, 0, 0, 8074.84, -3840, 690.061, 4.6, 'Shy-Rotam - Between 0-50% health - Summon Creature Sian-Rotam (No Repeat)'),
+--
+(14372, 0, 0, 0, 1, 0, 100, 0, 4000, 4000, 4000, 4000, 0, 0, 11, 17205, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,  'Winterfall Ambusher - OOC - Cast Winterfall Firewater');
