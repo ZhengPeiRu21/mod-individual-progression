9daa0f0557898d0b905804a092477444969f35e8
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-22 20:26:36 +0200

    Update dungeon_blackwing_lair.sql
    add smart script for Blackwing Taskmaster

--- sql/world/base/dungeon_blackwing_lair.sql
+++ sql/world/base/dungeon_blackwing_lair.sql
@@ -1,4 +1,15 @@
-    
+-- smart scripts
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (12458);
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` IN (12458);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
+`action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
+`target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+--
+(12458, 0, 0, 0, 1, 0, 100, 0, 15000, 35000, 15000, 35000, 0, 0, 11, 22458, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Blackwing Taskmaster - OOC - Cast Healing Circle'),
+(12458, 0, 1, 0, 0, 0, 100, 0, 1000, 6000, 8000, 12000, 0, 0, 11, 17289, 0, 0, 0, 0, 0, 2, 20, 0, 0, 0, 0, 0, 0, 0,   'Blackwing Taskmaster - In Combat - Cast Shadow Shock');
+
+
 DELETE FROM `spell_script_names` WHERE `spell_id` = 23310 AND `ScriptName` = 'spell_bwl_chromaggus_time_lapse';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (23310, 'spell_bwl_chromaggus_time_lapse');
