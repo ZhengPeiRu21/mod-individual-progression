e64dc8bee1a46d03d9d9745f4c4b902580e38d63
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-23 04:49:37 +0200

    Update zone_orgrimmar.sql
    add smart script for Vol'jin

--- sql/world/base/zone_orgrimmar.sql
+++ sql/world/base/zone_orgrimmar.sql
@@ -582,3 +582,13 @@ UPDATE `quest_template` SET
                       You'll find Neeru in the Cleft of Shadow." WHERE `ID` = 3121;
 
 UPDATE `creature_questender`   SET `id` = 3216 WHERE `quest` = 3121;
+
+-- smart scripts
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (10540);
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` IN (10540);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
+`action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
+`target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+--
+(10540, 0, 0, 0, 4, 0, 100, 1, 0, 0, 0, 0, 0, 0, 4, 5881, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Vol\'jin - On Aggro - Play Sound (No Repeat)');
