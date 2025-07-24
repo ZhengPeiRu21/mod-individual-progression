e472721de6692dc790b80a4f7c6056e987773cc7
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-24 12:28:38 +0200

    Update dungeon_uldaman.sql
    test done

--- sql/world/base/dungeon_uldaman.sql
+++ sql/world/base/dungeon_uldaman.sql
@@ -7,6 +7,5 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
 `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 --
--- (7078, 0, 0, 0, 9, 0, 100, 0, 3500, 8900, 11300, 38900, 0, 5, 11, 744, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Cleft Scorpid - Within 0-5 Range - Cast Poison)');
 (7078, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 3616, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Cleft Scorpid        - On Reset - Cast Poison Proc'),
 (7405, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 3616, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Deadly Cleft Scorpid - On Reset - Cast Poison Proc');
