7d6e2b466758b19085c82d05b43fb98b4be02df6
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-07-24 03:52:34 +0200

    Update zone_barrens.sql
    tests done

--- sql/world/base/zone_barrens.sql
+++ sql/world/base/zone_barrens.sql
@@ -138,17 +138,16 @@ INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Lan
 
 -- smart scripts
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (3244, 3245, 3246, 3279, 3398, 3634);
-
 DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` IN (3244, 3245, 3246, 3279, 3398, 3634);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
 `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
 `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
 `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
     
-(3244, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,          'Greater Plainstrider  - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(3245, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,          'Ornery Plainstrider   - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(3246, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,          'Fleeting Plainstrider - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(3279, 0, 0, 0, 4, 0, 20, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,             'Witchwing Ambusher    - On Aggro - Say Line 0'),
-(3398, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 3583, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,       'Gesharahan            - On Reset - Cast Deadly Poison'),
-(3634, 0, 0, 0, 1, 0, 100, 1, 1000, 1000, 0, 0, 0, 0, 11, 22766, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Deviate Stalker       - Out of Combat - Cast Sneak');
+(3244, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                    'Greater Plainstrider  - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(3245, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                    'Ornery Plainstrider   - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(3246, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                    'Fleeting Plainstrider - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(3279, 0, 0, 0, 4, 0, 20, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,                       'Witchwing Ambusher    - On Aggro - Say Line 0'),
+(3398, 0, 0, 0, 9, 0, 100, 0, 5000, 8000, 120000, 130000, 0, 10, 11, 3583, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Gesharahan            - Within 0-10 Range - Cast Deadly Poison'),
+(3634, 0, 0, 0, 1, 0, 100, 1, 1000, 1000, 0, 0, 0, 0, 11, 22766, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,           'Deviate Stalker       - Out of Combat - Cast Sneak');
     
