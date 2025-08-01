d6bb628b98c44533b1f302e0049d3e20ddb9a74e
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-08-01 18:13:53 +0200

    Update zone_thousand_needles.sql

--- sql/world/base/zone_thousand_needles.sql
+++ sql/world/base/zone_thousand_needles.sql
@@ -9,6 +9,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (5937, 0, 0, 0, 9, 0, 100, 0, 1000, 11000, 16000, 24000, 0, 5, 11, 8257, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Vile Sting - Within 0-5 Range - Cast Venom Sting'),
 (10882, 0, 0, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 17213, 0, 0, 0, 0, 0, 5, 30, 0, 0, 0, 0, 0, 0, 0,            'Arikara - On Spawn - Cast Curse of Vengeance');
 
+-- Pooled Treasure Chests
+DELETE FROM `pool_template` WHERE `entry` IN (66001);
+INSERT INTO `pool_template` (`entry`,`max_limit`,`description`) VALUES
+(66001, 1, 'Treasures - Thousand Needles');
+
+UPDATE `gameobject` SET `spawntimesecs` = 900 WHERE `guid` IN (16718, 85670, 87381);
+
 DELETE FROM `pool_gameobject` WHERE `pool_entry` = 66001;
 INSERT INTO `pool_gameobject` (`guid`, `pool_entry`, `chance`, `description`) VALUES 
 (16718, 66001, 0, 'Solid Chest, Camp E\'thok, node 1'),
