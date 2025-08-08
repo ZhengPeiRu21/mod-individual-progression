9d8641833c3b3d04b49678e9bcb9a2b14abc8be8
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-08-08 09:41:24 +0200

    Update zone_arathi_highlands.sql

--- sql/world/base/zone_arathi_highlands.sql
+++ sql/world/base/zone_arathi_highlands.sql
@@ -1,31 +1,66 @@
 -- smart scripts
+UPDATE `creature_template` SET `AIName` = '' WHERE `entry` IN (2581);
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN 
-(2554, 2555, 2556, 2562, 2563, 2564);
+(2554, 2555, 2556, 2560, 2561, 2562, 2563, 2564, 2565, 2566, 2567, 2580, 2582, 2586, 2587, 2589, 2604, 2793, 4479, 4480, 4481);
 DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` IN 
-(2554, 2555, 2556, 2562, 2563, 2564);
+(2554, 2555, 2556, 2560, 2561, 2562, 2563, 2564, 2565, 2566, 2567, 2580, 2581, 2582, 2586, 2587, 2589, 2604, 2793, 4479, 4480, 4481);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
 `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
 `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
 `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 --
-(2554, 0, 0, 0, 9, 0, 100, 0, 0, 0, 2000, 4000, 5, 30, 11, 10277, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,         'Witherbark Axe Thrower - Within 5-30 Range - Cast Throw'),
-(2554, 0, 1, 0, 9, 0, 100, 0, 10200, 26000, 2500, 6700, 0, 5, 11, 4974, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,   'Witherbark Axe Thrower - Within 0-5 Range - Cast Wither Touch'),
-(2554, 0, 2, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                    'Witherbark Axe Thrower - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(2555, 0, 0, 0, 9, 0, 100, 0, 6500, 15300, 80900, 93900, 0, 5, 11, 5605, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,   'Witherbark Witch Doctor - Within 0-5 Range - Cast Healing Ward'),
-(2555, 0, 1, 0, 9, 0, 100, 0, 0, 16000, 11100, 23400, 0, 8, 11, 8190, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,      'Witherbark Witch Doctor - Within 0-8 Range - Cast Magma Totem'),
-(2555, 0, 2, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                    'Witherbark Witch Doctor - Between 0-15% Health - Flee For Assist (No Repeat)'),
-(2556, 0, 0, 0, 9, 0, 100, 0, 9000, 24000, 18500, 39800, 0, 20, 11, 6533, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,  'Witherbark Headhunter - Within 0-20 Range - Cast Net'),
-(2556, 0, 1, 0, 9, 0, 100, 0, 20200, 31400, 38500, 47500, 0, 5, 11, 4974, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Witherbark Headhunter - Within 0-5 Range - Cast Wither Touch'),
-(2556, 0, 2, 0, 12, 0, 100, 1, 0, 20, 0, 0, 0, 0, 11, 7160, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,                'Witherbark Headhunter - Target Between 0-20% Health - Cast Execute (No Repeat)'),
-(2556, 0, 3, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                    'Witherbark Headhunter - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(2554, 0, 0, 0, 9, 0, 100, 0, 0, 0, 2000, 4000, 5, 30, 11, 10277, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,          'Witherbark Axe Thrower - Within 5-30 Range - Cast Throw'),
+(2554, 0, 1, 0, 9, 0, 100, 0, 10200, 26000, 2500, 6700, 0, 5, 11, 4974, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,    'Witherbark Axe Thrower - Within 0-5 Range - Cast Wither Touch'),
+(2554, 0, 2, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Witherbark Axe Thrower - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(2555, 0, 0, 0, 9, 0, 100, 0, 6500, 15300, 80900, 93900, 0, 5, 11, 5605, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,    'Witherbark Witch Doctor - Within 0-5 Range - Cast Healing Ward'),
+(2555, 0, 1, 0, 9, 0, 100, 0, 0, 16000, 11100, 23400, 0, 8, 11, 8190, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,       'Witherbark Witch Doctor - Within 0-8 Range - Cast Magma Totem'),
+(2555, 0, 2, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Witherbark Witch Doctor - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(2556, 0, 0, 0, 9, 0, 100, 0, 9000, 24000, 18500, 39800, 0, 20, 11, 6533, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,   'Witherbark Headhunter - Within 0-20 Range - Cast Net'),
+(2556, 0, 1, 0, 9, 0, 100, 0, 20200, 31400, 38500, 47500, 0, 5, 11, 4974, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,  'Witherbark Headhunter - Within 0-5 Range - Cast Wither Touch'),
+(2556, 0, 2, 0, 12, 0, 100, 1, 0, 20, 0, 0, 0, 0, 11, 7160, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,                 'Witherbark Headhunter - Target Between 0-20% Health - Cast Execute (No Repeat)'),
+(2556, 0, 3, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Witherbark Headhunter - Between 0-15% Health - Flee For Assist (No Repeat)'),
 --
-(2562, 0, 0, 0, 4, 0, 5, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,                        'Boulderfist Ogre - On Aggro - Say Line 0 (No Repeat)'),
-(2562, 0, 1, 0, 2, 0, 100, 0, 0, 30, 15300, 42700, 0, 0, 11, 4955, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,         'Boulderfist Ogre - Between 0-30% Health - Cast Fist of Stone'),
-(2563, 0, 0, 0, 9, 0, 100, 0, 9000, 34000, 19000, 38000, 0, 20, 11, 4962, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,  'Plains Creeper - Within 0-20 Range - Cast Encasing Webs'),
-(2563, 0, 1, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 3616, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                 'Plains Creeper - On Respawn - Cast Poison Proc'),
-(2564, 0, 0, 0, 4, 0, 5, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,                        'Boulderfist Enforcer - On Aggro - Say Line 0 (No Repeat)'),
-(2564, 0, 1, 0, 9, 0, 100, 0, 4000, 13000, 30000, 40000, 0, 10, 11, 13730, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boulderfist Enforcer - Within 0-10 Range - Cast Demoralizing Shout'),
-(2564, 0, 2, 0, 2, 0, 100, 0, 0, 30, 27200, 46600, 0, 0, 11, 4955, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,         'Boulderfist Enforcer - Between 0-30% Health - Cast Fist of Stone');
+(2560, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 8876, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                  'Highland Thrasher - On Reset - Cast Thrash Proc'),
+(2561, 0, 0, 0, 2, 0, 100, 0, 0, 50, 12000, 24000, 0, 0, 11, 3393, 0, 0, 0, 0, 0, 5, 5, 0, 0, 0, 0, 0, 0, 0,          'Highland Fleshstalker - Within 0-5 Range - Cast Consume Flesh'),
+(2562, 0, 0, 0, 4, 0, 5, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,                         'Boulderfist Ogre - On Aggro - Say Line 0 (No Repeat)'),
+(2562, 0, 1, 0, 2, 0, 100, 0, 0, 30, 15300, 42700, 0, 0, 11, 4955, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,          'Boulderfist Ogre - Between 0-30% Health - Cast Fist of Stone'),
+(2563, 0, 0, 0, 9, 0, 100, 0, 9000, 34000, 19000, 38000, 0, 20, 11, 4962, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,   'Plains Creeper - Within 0-20 Range - Cast Encasing Webs'),
+(2563, 0, 1, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 3616, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                  'Plains Creeper - On Respawn - Cast Poison Proc'),
+(2564, 0, 0, 0, 4, 0, 5, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,                         'Boulderfist Enforcer - On Aggro - Say Line 0 (No Repeat)'),
+(2564, 0, 1, 0, 9, 0, 100, 0, 4000, 13000, 30000, 40000, 0, 10, 11, 13730, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,  'Boulderfist Enforcer - Within 0-10 Range - Cast Demoralizing Shout'),
+(2564, 0, 2, 0, 2, 0, 100, 0, 0, 30, 27200, 46600, 0, 0, 11, 4955, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,          'Boulderfist Enforcer - Between 0-30% Health - Cast Fist of Stone'),
+(2565, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 3616, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,                 'Giant Plains Creeper - On Reset - Cast Poison Proc'),
+(2565, 0, 1, 0, 9, 0, 100, 0, 9000, 34000, 19000, 38000, 0, 20, 11, 4962, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,   'Giant Plains Creeper - Within 0-20 Range - Cast Encasing Webs'),
+(2566, 0, 0, 0, 4, 0, 5, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,                         'Boulderfist Brute - On Aggro - Say Line 0 (No Repeat)'),
+(2566, 0, 1, 0, 9, 0, 100, 0, 5000, 11000, 12000, 17000, 0, 5, 11, 11428, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,   'Boulderfist Brute - Within 0-5 Range - Cast Knockdown'),
+(2566, 0, 2, 0, 2, 0, 100, 1, 0, 30, 0, 0, 0, 0, 11, 4955, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                  'Boulderfist Brute - Between 0-30% Health - Cast Fist of Stone (No Repeat)'),
+(2567, 0, 0, 0, 1, 0, 100, 1, 1000, 1000, 0, 0, 0, 0, 11, 12544, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,            'Boulderfist Magus - Out of Combat - Cast Frost Armor (No Repeat)'),
+(2567, 0, 1, 0, 4, 0, 15, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,                        'Boulderfist Magus - On Aggro - Say Line 0'),
+(2567, 0, 2, 0, 0, 0, 100, 0, 0, 0, 3500, 6400, 0, 0, 11, 20822, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,           'Boulderfist Magus - In Combat - Cast Frostbolt'),
+(2567, 0, 3, 0, 106, 0, 100, 0, 6000, 9800, 15400, 23500, 0, 10, 11, 11831, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boulderfist Magus - Within 0-10 Range - Cast Frost Nova'),
+(2567, 0, 4, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Boulderfist Magus - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(2580, 0, 0, 0, 0, 0, 100, 0, 9000, 15000, 39000, 65000, 0, 0, 11, 8139, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,   'Elder Mesa Buzzard - In Combat - Cast Fevered Fatigue'),
+(2582, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Dabyrie Laborer - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(2586, 0, 0, 0, 1, 0, 100, 1, 1000, 1000, 0, 0, 0, 0, 11, 22766, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,            'Syndicate Highwayman - Out of Combat - Cast Sneak (No Repeat)'),
+(2586, 0, 1, 0, 4, 0, 100, 0, 0, 0, 0, 0, 0, 0, 28, 22766, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                  'Syndicate Highwayman - On Aggro - Remove Aura Sneak'),
+(2586, 0, 2, 0, 67, 0, 100, 0, 1000, 3000, 4000, 7000, 0, 5, 11, 2591, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,      'Syndicate Highwayman - On Behind Target - Cast Backstab'),
+(2586, 0, 3, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Syndicate Highwayman - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(2587, 0, 0, 0, 9, 0, 100, 0, 0, 0, 2000, 4000, 5, 30, 11, 6660, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,           'Syndicate Pathstalker - Within 5-30 Range - Cast Shoot'),
+(2587, 0, 1, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Syndicate Pathstalker - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(2589, 0, 0, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Syndicate Mercenary - Between 0-15% Health - Flee For Assist'),
+--
+(2604, 0, 0, 0, 9, 0, 80, 0, 9000, 13000, 6000, 16000, 0, 5, 11, 6253, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,      'Molok the Crusher - In Combat - Cast Backhand'),
+(2793, 0, 0, 0, 4, 0, 30, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,                        'Kor gresh Coldrage - On Aggro - Say Line 0 (No Repeat)'),
+(2793, 0, 1, 0, 106, 0, 100, 0, 8000, 12000, 48000, 53000, 0, 10, 11, 865, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,  'Kor gresh Coldrage - Within 0-10 Range - Cast Frost Nova'),
+(2793, 0, 2, 0, 0, 0, 100, 0, 1000, 7000, 48000, 55000, 0, 0, 11, 4320, 64, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,    'Kor gresh Coldrage - In Combat - Cast Trelanes Freezing Touch'),
+(4479, 0, 0, 0, 9, 0, 100, 0, 3000, 6000, 12000, 13000, 0, 10, 11, 3583, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,   'Fardel Dabyrie - Within 0-10 Range - Cast Deadly Poison'),
+(4479, 0, 1, 0, 2, 0, 100, 1, 0, 20, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Fardel Dabyrie - Between 0-20% Health - Flee For Assist'),
+(4480, 0, 0, 0, 9, 0, 100, 0, 2000, 4000, 6000, 12000, 0, 5, 11, 8379, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,      'Kenata Dabyrie - Within 0-5 Range - Cast Disarm'),
+(4480, 0, 1, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Kenata Dabyrier - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(4481, 0, 0, 0, 4, 0, 100, 1, 0, 0, 0, 0, 0, 0, 11, 7164, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                   'Marcel Dabyrie - On Aggro - Cast Defensive Stance'),
+(4481, 0, 1, 0, 105, 0, 100, 0, 6000, 11000, 19000, 25000, 0, 5, 11, 11972, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Marcel Dabyrie - In Combat - Cast Shield Bash'),
+(4481, 0, 2, 0, 2, 0, 100, 1, 0, 50, 0, 0, 0, 0, 11, 15062, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                 'Marcel Dabyrie - Between 0-50% Health - Cast Shield Wall (No Repeat)'),
+(4481, 0, 3, 0, 2, 0, 100, 1, 0, 30, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Marcel Dabyrie - Between 0-30% Health - Flee For Assist (No Repeat)');
 
 
 -- remove non elite Syndicate thief from Stromgarde
