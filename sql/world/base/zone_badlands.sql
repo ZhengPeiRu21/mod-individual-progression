085c021196e176f6f3f91caa98dd9ed75d1113d2
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-08-13 07:04:48 +0200

    Update zone_badlands.sql

--- sql/world/base/zone_badlands.sql
+++ sql/world/base/zone_badlands.sql
@@ -1,9 +1,9 @@
 /* smart scripts */
-UPDATE `creature_template` SET `AIName` = '' WHERE `entry` IN (2754);
+UPDATE `creature_template` SET `AIName` = '' WHERE `entry` IN (2753, 2754);
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN 
-(2716, 2717, 2718, 2719, 2720, 2893, 2894, 2907);
+(2716, 2717, 2718, 2719, 2720, 2739, 2740, 2742, 2743, 2893, 2894, 2907);
 DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` IN 
-(2716, 2717, 2718, 2719, 2720, 2754, 2893, 2894, 2907);
+(2716, 2717, 2718, 2719, 2720, 2739, 2740, 2742, 2743, 2753, 2754, 2893, 2894, 2907);
 
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
 `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
@@ -27,6 +27,18 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (2720, 0, 0, 0, 4, 0, 15, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,                        'Dustbelcher Ogre Mage - On Aggro - Say Line 0'),
 (2720, 0, 1, 0, 0, 0, 100, 0, 0, 0, 3600, 5100, 0, 0, 11, 20822, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,           'Dustbelcher Ogre Mage - In Combat - Cast Frostbolt'),
 (2720, 0, 2, 0, 0, 0, 100, 0, 3000, 8000, 30000, 45000, 0, 0, 11, 6742, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,     'Dustbelcher Ogre Mage - In Combat - Cast Bloodlust'),
+(2739, 0, 0, 0, 4, 0, 100, 1, 0, 0, 0, 0, 0, 0, 11, 7164, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                   'Shadowforge Tunneler - On Aggro - Cast Defensive Stance'),
+(2739, 0, 1, 0, 9, 0, 100, 0, 0, 0, 7600, 13700, 0, 5, 11, 8380, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,            'Shadowforge Tunneler - In Combat - Cast Sunder Armor'),
+(2739, 0, 2, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Shadowforge Tunneler - Between 0-15% Health - Flee For Assist'),
+(2740, 0, 0, 0, 0, 0, 100, 0, 0, 0, 3500, 4000, 0, 0, 11, 20825, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,           'Shadowforge Darkweaver - In Combat - Cast Shadow Bolt'),
+(2740, 0, 1, 0, 0, 0, 100, 0, 14900, 23100, 14900, 31200, 0, 0, 11, 9482, 33, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,  'Shadowforge Darkweaver - In Combat - Cast Amplify Flames'),
+(2740, 0, 2, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Shadowforge Darkweaver - Between 0-15% Health - Flee For Assist (No Repeat)'),
+(2742, 0, 0, 0, 0, 0, 100, 0, 8800, 11900, 16400, 35800, 0, 0, 11, 6066, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,    'Shadowforge Chanter - In Combat - Cast Power Word: Shield'),
+(2742, 0, 1, 0, 74, 0, 100, 0, 0, 0, 18800, 36600, 40, 40, 11, 6077, 65, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,       'Shadowforge Chanter - On Friendly Between 0-40% Health - Cast Renew'),
+(2742, 0, 2, 0, 0, 0, 100, 0, 8000, 10000, 18000, 24000, 0, 0, 11, 15970, 65, 0, 0, 0, 0, 5, 30, 0, 0, 0, 0, 0, 0, 0, 'Shadowforge Chanter - In Combat - Cast Sleep'),
+(2742, 0, 3, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Shadowforge Chanter - Between 0-15% Health - Flee For Assist'),
+(2743, 0, 0, 0, 0, 0, 100, 0, 7000, 18000, 22000, 23000, 0, 0, 11, 17207, 0, 0, 0, 0, 0, 21, 5, 0, 0, 0, 0, 0, 0, 0,  'Shadowforge Warrior - In Combat - Cast Whirlwind'),
+(2743, 0, 1, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Shadowforge Warrior - Between 0-15% Health - Flee For Assist'),
 (2893, 0, 0, 0, 0, 0, 100, 0, 4000, 15900, 15700, 26100, 0, 0, 11, 11976, 0, 0, 0, 0, 0, 21, 5, 0, 0, 0, 0, 0, 0, 0,  'Stonevault Bonesnapper - Within 0-5 Range - Cast Strike'),
 (2893, 0, 1, 0, 105, 0, 100, 0, 9000, 19000, 15000, 25000, 0, 5, 11, 12555, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Stonevault Bonesnapper - Within 0-5 Range - Cast Pummel'),
 (2893, 0, 2, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                     'Stonevault Bonesnapper - Between 0-15% Health - Flee For Assist'),
