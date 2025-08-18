fe82954428157e09f6a9713ac5dddbf127ea014e
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-08-18 07:03:14 +0200

    Update instance_onyxias_lair.cpp

--- src/vanillaScripts/instance_onyxias_lair.cpp
+++ src/vanillaScripts/instance_onyxias_lair.cpp
@@ -150,9 +150,9 @@ public:
         if (player->GetLevel() < IP_LEVEL_TBC && (player->HasItemCount(ITEM_DRAKEFIRE_AMULET) ||  isExcludedFromProgression(player)))
         {
             player->SetRaidDifficulty(RAID_DIFFICULTY_10MAN_HEROIC);
-            player->TeleportTo(249, 29.1607f, -71.3372f, -8.18032f, 4.58f);       
+            player->TeleportTo(249, 29.1607f, -71.3372f, -8.18032f, 4.58f);
         }
-        return true;   
+        return true;
     }
 };
 
