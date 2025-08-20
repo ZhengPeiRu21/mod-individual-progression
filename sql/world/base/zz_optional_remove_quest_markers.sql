b265fcbe9cec83c58b720ae1ca26a273d8daa70e
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-08-20 13:58:09 +0200

    Delete sql/world/base/zz_optional_remove_quest_markers.sql

--- sql/world/base/zz_optional_remove_quest_markers.sql
+++
@@ -1,9 +0,0 @@
-/* These commands will remove quest markers from the map so that player need to read quest description. These markers were added in late WotLK. */
-/* This is the optional file with the most differing opinions from players on whether to enable or disable - older versions of the module had it disabled
-   by default. Because the opinion is so mixed, I have changed it to be enabled by default, as this is the most accurate to pre-3.2 WoW and the quests
-   were designed to be played without markers. But please feel free to comment out or delete this file if you prefer to have them! */
-
-DELETE FROM `gameobject_questitem` WHERE `GameObjectEntry` BETWEEN 32 AND 201937;
-DELETE FROM `quest_poi` WHERE `QuestID` BETWEEN 1 AND 25495;
-DELETE FROM `quest_poi_points` WHERE `QuestID` BETWEEN 1 AND 25495;
-DELETE FROM `creature_questitem` WHERE `CreatureEntry` BETWEEN 3 AND 39819;
