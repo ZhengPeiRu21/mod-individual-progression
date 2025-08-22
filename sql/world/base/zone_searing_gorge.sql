e4646eb0620b91f7eb4e182b704beff94e1001ce
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-08-22 11:35:24 +0200

    Update zone_searing_gorge.sql

--- sql/world/base/zone_searing_gorge.sql
+++ sql/world/base/zone_searing_gorge.sql
@@ -84,10 +84,11 @@ UPDATE `creature` SET `id1` = 8504 WHERE `guid` = 6832;
 /* Maltorius had 2 elite Dark Iron Sentries next to him that were replaced by a single non-elite dark iron lookout in 2.3 - restore the originals */
 UPDATE `creature` SET `id1` = 8504 WHERE `guid` = 5846;
 
-DELETE FROM `creature` WHERE `guid` = 608504;
+DELETE FROM `creature` WHERE `guid` IN (608504, 608505);
 INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, 
 `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES 
-(608504, 8504, 0, 0, 0, 0, 0, 1, 1, 1, -6630.98, -1233.1, 209.809, 1.29509, 300, 0, 0, 2578, 0, 0, 0, 0, 0, '', NULL, 0, NULL);
+(608504, 8504, 0, 0, 0, 0, 0, 1, 1, 1, -6630.98, -1233.1, 209.809, 1.29509, 300, 0, 0, 4950, 0, 0, 0, 0, 0, '', NULL, 0, NULL),
+(608505, 8504, 0, 0, 0, 0, 0, 1, 1, 1, -7023.45, -1283.97, 258.302, 4.8895, 300, 0, 0, 4950, 0, 0, 0, 0, 0, '', NULL, 0, NULL); -- 4th tower Dark Iron Sentry
 
 /* Group Maltorius together with the 2 sentries */
 DELETE FROM `creature_formations` WHERE `leaderGUID` = 5845;
