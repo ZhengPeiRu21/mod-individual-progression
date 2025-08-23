88c4eeb79fe0db61ab2aab116bea75101071eb64
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-08-23 04:35:10 +0200

    Update and rename sql/world/base/zone_silithus.sql to data/sql/world/base/zone_silithus.sql

--- data/sql/world/base/zone_silithus.sql
+++ data/sql/world/base/zone_silithus.sql
@@ -6,4 +6,4 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
 `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 --
-(15634, 0, 0, 0, 14, 0, 100, 0, 1000, 200, 50000, 60000, 0, 0, 11, 25839, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,   'Priestess of the Moon - On Friendly Health - Cast Mass Healing');
+(15634, 0, 0, 0, 14, 0, 100, 0, 1000, 200, 50000, 60000, 0, 0, 11, 25839, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,   'Priestess of the Moon - On Friendly Health - Cast Mass Healing');
