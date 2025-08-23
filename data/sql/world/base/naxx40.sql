774c12c045364add957b6d0a0eed1828d1145284
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-08-23 06:48:04 +0200

    Update naxx40.sql

--- data/sql/world/base/naxx40.sql
+++ data/sql/world/base/naxx40.sql
@@ -1,8 +1,7 @@
--- Custom GUIDs
--- creature 352042
--- creature_template 351097
--- Add MapDifficulty DBC Override
--- https://wow.tools/dbc/?dbc=mapdifficulty&build=10.0.0.44895#page=1&search=533
+-- Enable Naxxramas Map (mapID: 533)
+DELETE FROM `disables` WHERE `entry` = 533;
+
+-- Add MapDifficulty DBC Override - https://wow.tools/dbc/?dbc=mapdifficulty&build=10.0.0.44895#page=1&search=533
 DELETE FROM `mapdifficulty_dbc` WHERE `ID` = 754;
 INSERT INTO `mapdifficulty_dbc` (`ID`, `MapID`, `Difficulty`, `Message_Lang_enUS`, `Message_Lang_Mask`, `RaidDuration`, `MaxPlayers`, `Difficultystring`) VALUES
 (754, 533, 2, 'You must be level 60 and in a raid group to enter.', 16712190, 604800, 40, 'RAID_DIFFICULTY_40PLAYER');
@@ -24,19 +23,16 @@ INSERT INTO `areatrigger_scripts` (`entry`, `ScriptName`) VALUES
 (5193, 'naxx_northrend_entrance'),
 (5194, 'naxx_northrend_entrance');
 
--- Update spawnMask of all creatures to 10man + 25man
-UPDATE `creature` SET `spawnMask` = 3 WHERE `spawnMask` = 7 AND `map` = 533;
--- Update spawnMask of all gameobjects to all
-UPDATE `gameobject` SET `spawnMask` = 7 WHERE `spawnMask` = 3 AND `map` = 533;
--- Lich King uses same entry in Naxx WotLK and Naxx40 - Allow spawning in all versions
-UPDATE `creature` SET `spawnMask` = `spawnMask` | 4 WHERE `id1` = 16980;
--- Naxxramas Trigger (frogger) should also spawn in Naxx40
-UPDATE `creature` SET `spawnMask` = `spawnMask`| 4 WHERE `id1` = 16082;
--- Orb of Naxxramas does not exist in classic
-UPDATE `gameobject` SET `spawnMask` = 3 WHERE `id` IN (202278, 202277);
+UPDATE `creature` SET `spawnMask` = 3 WHERE `spawnMask` = 7 AND `map` = 533;   -- Update spawnMask of all creatures to 10man + 25man
+UPDATE `creature` SET `spawnMask` = `spawnMask`| 4 WHERE `id1` = 16980;        -- Lich King uses same entry in Naxx WotLK and Naxx40 - Allow spawning in all versions
+UPDATE `creature` SET `spawnMask` = `spawnMask`| 4 WHERE `id1` = 16082;        -- Naxxramas Trigger (frogger) should also spawn in Naxx40
 
-DELETE FROM `dungeon_access_template` WHERE `id`=122;
-INSERT INTO `dungeon_access_template` (`id`, `map_id`, `difficulty`, `min_level`, `max_level`, `min_avg_item_level`, `comment`) VALUES (122, 533, 2, 60, 0, 0, 'Naxxramas - 40man');
+UPDATE `gameobject` SET `spawnMask` = 7 WHERE `spawnMask` = 3 AND `map` = 533; -- Update spawnMask of all gameobjects to all
+UPDATE `gameobject` SET `spawnMask` = 3 WHERE `id` IN (202278, 202277);        -- Orb of Naxxramas does not exist in classic
+
+DELETE FROM `dungeon_access_template` WHERE `id` = 122;
+INSERT INTO `dungeon_access_template` (`id`, `map_id`, `difficulty`, `min_level`, `max_level`, `min_avg_item_level`, `comment`) VALUES 
+(122, 533, 2, 60, 0, 0, 'Naxxramas - 40man');
 
 -- Naxx40 Dungeon DBC entries (helps Autobalance work correctly)
 DELETE FROM `lfgdungeons_dbc` WHERE `ID` = 1001;
