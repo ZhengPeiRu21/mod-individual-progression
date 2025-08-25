e92b44f57668f94077e17753ad6e1820ea9d6e21
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-08-25 11:07:44 +0200

    Update aq_war.sql

--- data/sql/world/base/aq_war.sql
+++ data/sql/world/base/aq_war.sql
@@ -2,6 +2,17 @@ SET @IPPPHASE := 65536;
 SET @CGUID    := 650000;
 SET @OGUID    := 650000;
 
+/* smart scripts */
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (15634);
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` IN (15634);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
+`action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
+`target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+--
+(15634, 0, 0, 0, 14, 0, 100, 0, 1000, 200, 50000, 60000, 0, 0, 11, 25839, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0,   'Priestess of the Moon - On Friendly Health - Cast Mass Healing');
+
+
 DELETE FROM `creature_text` WHERE `CreatureID`= 15693;
 INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language`,`Probability`,`Emote`,`Duration`,`Sound`,`BroadcastTextId`, `TextRange`, `comment`) VALUES
 (15693, 0, 0, '$n, Champion of the Bronze Dragonflight, has rung the Scarab Gong. The ancient gates of Ahn\'Qiraj open, revealing the horrors of a forgotten war...', 16, 0, 100, 1, 0, 0, 11427, 4, 'EMOTE_AQ_GONG_1'),
