1d18c8f15d9db2ee67d845960e4601595d79eaba
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-09-24 10:32:54 +0200

    Update IndividualProgression.h

--- src/IndividualProgression.h
+++ src/IndividualProgression.h
@@ -268,6 +268,7 @@ public:
     void AdjustWotLKStats(Player* player) const;
     bool hasCustomProgressionValue(uint32 creatureEntry);
     void checkIPProgression(Player* player);	
+    void UpdateProgressionQuests(Player* player);
     void checkKillProgression(Player* player, Creature* killed);
     static void LoadCustomProgressionEntries(const std::string& customProgressionString);
     static void AdjustStats(Player* player, float computedAdjustment, float computedHealingAdjustment);
