f17399090bdf3473c7f29574ae6e5fd6e1fe0fe0
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-10-10 13:06:53 +0200

    Update cs_individualProgression.cpp

--- src/cs_individualProgression.cpp
+++ src/cs_individualProgression.cpp
@@ -77,7 +77,7 @@ public:
 
     static bool HandleTeleIndividualProgressionCommand(ChatHandler* handler, Optional<PlayerIdentifier> player, std::string location)
     {	 
-        if (location != "naxx40" && location != "onyxia40")
+        if (location != "naxx40" && location != "onyxia40" && location != "naxx" && location != "onyxia")
         {
             handler->PSendSysMessage("|cff00ffff{}|r is not a valid teleport location.", location);
             return false;
@@ -88,13 +88,13 @@ public:
         uint32 progressionLevel = target->GetPlayerSetting("mod-individual-progression", SETTING_PROGRESSION_STATE).value;
         std::string playername = target->GetName();
 
-        if (location == "naxx40" && ((progressionLevel < PROGRESSION_TBC_TIER_5 && isAttuned(target)) || target->IsGameMaster()))
+        if ((location == "naxx" || location == "naxx40" ) && ((progressionLevel < PROGRESSION_TBC_TIER_5 && isAttuned(target)) || target->IsGameMaster()))
         {
             target->SetRaidDifficulty(RAID_DIFFICULTY_10MAN_HEROIC);
             target->TeleportTo(533, 3005.51f, -3434.64f, 304.195f, 6.2831f);
             return true;
         }
-        else if (location == "onyxia40" && ((progressionLevel < PROGRESSION_TBC_TIER_5 && target->HasItemCount(ITEM_DRAKEFIRE_AMULET)) || target->IsGameMaster()))
+        else if ((location == "onyxia" || location == "onyxia40") && ((progressionLevel < PROGRESSION_TBC_TIER_5 && target->HasItemCount(ITEM_DRAKEFIRE_AMULET)) || target->IsGameMaster()))
         {
             target->SetRaidDifficulty(RAID_DIFFICULTY_10MAN_HEROIC);
             target->TeleportTo(249, 29.1607f, -71.3372f, -8.18032f, 4.58f);
