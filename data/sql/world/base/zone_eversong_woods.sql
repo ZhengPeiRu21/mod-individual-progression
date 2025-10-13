65242741a505e44277deb7a67e640df4deb67c64
Simon Hensel <80845904+sihensel@users.noreply.github.com>
2025-10-13 11:57:29 +0200

    Cleanup TBC creatures (#805)
    Remove duplicates and add missing creatures to tbc_creatures sql

--- data/sql/world/base/zone_eversong_woods.sql
+++ data/sql/world/base/zone_eversong_woods.sql
@@ -1,9 +1,3 @@
-UPDATE `creature_template` SET `faction` = 16 WHERE `entry` = 15271; -- Tender
-UPDATE `creature_template` SET `faction` = 16 WHERE `entry` = 15273; -- Arcane Wraith
-UPDATE `creature_template` SET `faction` = 16 WHERE `entry` = 15294; -- Feral Tender
-UPDATE `creature_template` SET `faction` = 16 WHERE `entry` = 15298; -- Tainted Arcane Wraith
-UPDATE `creature_template` SET `faction` = 14 WHERE `entry` = 15367; -- Felendren the Banished
-
 -- Windows to the Source (Warlock)
 DELETE FROM `creature_questender` WHERE `id`=15283 AND `quest`=8344;
 INSERT INTO `creature_questender` (`id`, `quest`) VALUES (15283, 8344);
