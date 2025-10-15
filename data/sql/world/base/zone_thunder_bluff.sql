c333d1674e144ef9ac13fd03bf5698a5504a004c
Simon Hensel <80845904+sihensel@users.noreply.github.com>
2025-10-16 01:33:22 +0200

    Cleanup Vanilla Vendors (#806)
    moving edits done to vanilla vendors in other sql files to vanilla_vendors
    
    ---------
    
    Co-authored-by: Grimfeather <88028633+Grimfeather@users.noreply.github.com>

--- data/sql/world/base/zone_thunder_bluff.sql
+++ data/sql/world/base/zone_thunder_bluff.sql
@@ -13,6 +13,8 @@ UPDATE `creature_template` SET `subname` = 'Expert Leatherworker'     WHERE `ent
 UPDATE `creature_template` SET `npcflag` = 81, `trainer_type` = 2 WHERE `entry` IN (3008, 10278, 11047, 11051, 11071, 11084);
 UPDATE `creature_template` SET `type_flags` = 134217728 WHERE `entry` IN (3004, 3007, 3009, 3011, 11084);
 
+/* NPC Thrumn - Remove non-Vanilla Tabards */
+DELETE FROM `npc_vendor` WHERE `entry`= 5189 AND `item` IN (15197, 15199, 19031, 19505, 24004, 31773, 31775, 31776, 31777, 31778, 31779, 31780, 31781, 31804, 32445, 32828, 35221);
 
 DELETE FROM `npc_trainer` WHERE `ID` IN (2798, 2998, 3001, 3004, 3007, 3008, 3009, 3011, 3013, 3026, 3028, 7089, 10278, 11047, 11051, 11071, 11084);
 INSERT INTO `npc_trainer` (`ID`, `SpellID`) VALUES 
@@ -76,40 +78,6 @@ UPDATE `creature_template` SET `subname` = 'Reagent Vendor'            WHERE `en
 UPDATE `creature_template` SET `subname` = 'General Goods Vendor'      WHERE `entry` = 8362;-- Kuruk <General Goods Vendor>
 UPDATE `creature_template` SET `subname` = 'Trade Goods Supplier'      WHERE `entry` = 8363;-- Shadi Mistrunner <Trade Goods Supplier>
 
-
--- Kurm Stonehoof <Mining Supplier>
-DELETE FROM `npc_vendor` WHERE `entry`=3002 AND `item` IN (20815, 20824);
-
--- Fyr Mistrunner <Bread Vendor>
-DELETE FROM `npc_vendor` WHERE `entry`=3003 AND `item` IN (27855, 33449, 35950);
-
--- Mahu <Leatherworking & Tailoring Supplies>
-DELETE FROM `npc_vendor` WHERE `entry`=3005 AND `item`=38426;
-
--- Nida Winterhoof <Herbalism Supplier>
-DELETE FROM `npc_vendor` WHERE `entry`=3014 AND `item`=40411;
-
--- Kuna Thunderhorn <Bowyer & Fletching Goods>
-DELETE FROM `npc_vendor` WHERE `entry`=3015 AND `item`=28053;
-
--- Nata Dawnstrider <Enchanting Supplies>
-DELETE FROM `npc_vendor` WHERE `entry`=3012 AND `item`=6342;
-INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES (3012, 6342, 1, 7200);
-
--- Kaga Mistrunner <Meat Vendor>
-DELETE FROM `npc_vendor` WHERE `entry`=3025 AND `item` IN (27854, 33454, 35953);
-
--- Naal Mistrunner <Cooking Supplier>
-DELETE FROM `npc_vendor` WHERE `entry`=3027 AND `item`=30817;
-
--- Thrumn <Tabard Vendor>
-DELETE FROM `npc_vendor` WHERE `entry`=5189 AND `item` IN (15197, 15199, 19031, 19505, 24004, 31773, 31775, 31776, 31777, 31778, 31779, 31780, 31781, 31804, 32445, 32828, 35221);
-
--- Innkeeper Pala <Innkeeper>
-DELETE FROM `npc_vendor` WHERE `entry`=6746 AND `item` IN (27857, 28399, 33443, 33444, 33445, 35952, 35954);
-
--- Chepi <Reagent Vendor>
-DELETE FROM `npc_vendor` WHERE `entry`=8361 AND `item` IN (22147, 22148, 37201, 44605, 44614, 44615);
 
 -- Evergreen Herb Casing should drop random herbs - was changed to WotLK to drop only Morrowgrain and Mutated Morrowgrain
 DELETE FROM `item_loot_template` WHERE `Entry` = 11024;
