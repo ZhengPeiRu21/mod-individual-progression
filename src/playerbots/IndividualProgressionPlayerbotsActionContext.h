6c8923eeac3c3fe7a08a9e1233a7283d65c4dd23
Keleborn <22352763+Celandriel@users.noreply.github.com>
2025-10-24 13:15:14 -0700

    Feat. Playerbots naxx 40 Integration (#749)
    Override default Naxx playerbots strategy to include Naxx40 bosses.

--- src/playerbots/IndividualProgressionPlayerbotsActionContext.h
+++ src/playerbots/IndividualProgressionPlayerbotsActionContext.h
@@ -6,6 +6,9 @@
 #include "IndividualProgressionOnyxiaActions.h"
 #include "RaidOnyxiaActionContext.h"
 
+#include "IndividualProgressionNaxxActions.h"
+#include "RaidNaxxActionContext.h"
+
 class IPOnyxiaActionContext : public RaidOnyxiaActionContext
 {
 public:
@@ -17,6 +20,21 @@ private:
     static Action* kill_playerbots::ActionRegistry::RegisterAction("ony kill whelps", []() { return new IPOnyxiaKillWhelpsAction(ai); }
 
 };
+
+class IPNaxxActionContext : public RaidNaxxActionContext
+{
+public:
+    IPNaxxActionContext : public RaidNaxxActionContext()
+    {
+        creators["grobbulus go behind"] = &IPNaxxActionContext::go_behind_the_boss;
+        creators["rotate grobbulus"] = &IPNaxxActionContext::rotate_grobbulus;
+    }
+
+private:
+    static Action* go_behind_the_boss(PlayerbotAI* ai) { return new IPGrobbulusGoBehindAction(ai); }
+    static Action* rotate_grobbulus(PlayerbotAI* ai) { return new IPGrobbulusRotateAction(ai); }
+};
+
 
 #endif
 #endif
