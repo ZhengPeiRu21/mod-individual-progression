4989e044f8785dd210eade3b26e4b3dba822bf59
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-11-18 09:35:45 +0100

    Update IndividualProgression_loader.cpp

--- src/IndividualProgression_loader.cpp
+++ src/IndividualProgression_loader.cpp
@@ -36,6 +36,7 @@ void AddSC_custom_gameobjects_40();
 void AddSC_custom_scripts_40();
 void AddSC_Ipp_Wotlk_Modded_Scripts();
 void AddSC_aq_scripts();
+void AddSC_aq_we_scripts();
 void AddSC_cot_scripts();
 void AddSC_misc_scripts();
 void AddSC_ipp_spell_scripts();
@@ -82,6 +83,7 @@ void Addmod_individual_progressionScripts()
     AddSC_custom_scripts_40();
     AddSC_Ipp_Wotlk_Modded_Scripts();
     AddSC_aq_scripts();
+    AddSC_aq_we_scripts();
     AddSC_cot_scripts();
     AddSC_misc_scripts();
     AddSC_ipp_spell_scripts();
