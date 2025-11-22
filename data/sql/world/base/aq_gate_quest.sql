f35b3a0e15fa6f3fd3bfb2b8f58490a8cd92b762
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-11-22 10:09:19 +0100

    Update aq_gate_quest.sql

--- data/sql/world/base/aq_gate_quest.sql
+++ data/sql/world/base/aq_gate_quest.sql
@@ -1,30 +1,15 @@
--- Gate of Ahn'Qiraj
-DELETE FROM `gameobject` WHERE `id` = 176146;
-INSERT INTO `gameobject` (`id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES (176146, 1, -8133.34, 1525.13, 17.1904, 6.28103, 0.00107995, -0.999999, 600, 100, 1);
-
--- Ahn'Qiraj Gate Roots
-DELETE FROM `gameobject` WHERE `id` = 176147;
-INSERT INTO `gameobject` (`id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES (176147, 1, -8133.34, 1525.13, 17.1904, 6.28103, 0.00107995, -0.999999, 600, 100, 1);
-
--- Ahn'Qiraj Gate Runes
-DELETE FROM `gameobject` WHERE `id` = 176148;
-INSERT INTO `gameobject` (`id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES (176148, 1, -8133.34, 1525.13, 17.1904, 6.28103, 0.00107995, -0.999999, 600, 100, 1);
+DELETE FROM `gameobject` WHERE `id` IN (176146, 176147, 176148);
+INSERT INTO `gameobject` (`id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES 
+(176146, 1, -8133.34, 1525.13, 17.1904, 6.28103, 0.00107995, -0.999999, 600, 100, 1), -- Gate of Ahn'Qiraj
+(176147, 1, -8133.34, 1525.13, 17.1904, 6.28103, 0.00107995, -0.999999, 600, 100, 1), -- Ahn'Qiraj Gate Roots
+(176148, 1, -8133.34, 1525.13, 17.1904, 6.28103, 0.00107995, -0.999999, 600, 100, 1); -- Ahn'Qiraj Gate Runes
 
 -- Ghost Gate - should not be placed, remove it if it is present
 DELETE FROM `gameobject` WHERE `id` = 180322;
-# INSERT INTO `gameobject` (`id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES (180322, 1, -8132.76, 1525.19, 6.19048, 6.27739, 0.00289526, -0.999996, 25, 100, 1);
 
--- Gate of Ahn'Qiraj
-UPDATE `gameobject_template_addon` SET `flags` = 4 WHERE `entry` = 176146;
-UPDATE `gameobject_template` SET `ScriptName` = 'aq_gate' WHERE  `entry` = 176148;
-UPDATE `gameobject_template` SET `ScriptName` = 'aq_gate' WHERE  `entry` = 176147;
-UPDATE `gameobject_template` SET `ScriptName` = 'aq_gate' WHERE  `entry` = 176146;
-
--- Ahn'Qiraj Gate Roots
-UPDATE `gameobject_template_addon` SET `flags` = 4 WHERE `entry` = 176147;
-
--- Ahn'Qiraj Gate Runes
-UPDATE `gameobject_template_addon` SET `flags` = 4 WHERE `entry` = 176148;
+-- Ahn'Qiraj Gate, Roots and Runes
+UPDATE `gameobject_template_addon` SET `flags` = 4 WHERE `entry` IN (176146, 176147, 176148);
+UPDATE `gameobject_template` SET `ScriptName` = 'aq_gate' WHERE `entry` IN (176146, 176147, 176148);
 
 -- fix tiny red crystal quest not completing
 UPDATE `gameobject_template` SET `ScriptName` = 'gobject_crystalline_tear' WHERE `entry` = 180633; -- was go_crystalline_tear
