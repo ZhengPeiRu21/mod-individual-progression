8c75d9a2924e6bc31d353ccb51ec0b2f6c12f809
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-12-04 16:36:01 +0100

    Update boss_maexxna_40.h

--- src/naxx40Scripts/boss_maexxna_40.h
+++ src/naxx40Scripts/boss_maexxna_40.h
@@ -9,19 +9,12 @@
 #include "SpellScript.h"
 #include "SpellScriptLoader.h"
 #include "naxxramas.h"
+#include "../../../../src/server/scripts/Northrend/Naxxramas/boss_maexxna.h"
 
 namespace Maexxna_40 {
 
 enum Spells
 {
-    SPELL_WEB_SPRAY_10                  = 29484,
-    SPELL_WEB_SPRAY_25                  = 54125,
-    SPELL_POISON_SHOCK_10               = 28741,
-    SPELL_POISON_SHOCK_25               = 54122,
-    SPELL_NECROTIC_POISON_10            = 54121,
-    SPELL_NECROTIC_POISON_25            = 28776,
-    SPELL_FRENZY_10                     = 54123,
-    SPELL_FRENZY_25                     = 54124,
     SPELL_WEB_WRAP_STUN                 = 28622,
     SPELL_WEB_WRAP_SUMMON               = 28627,
     SPELL_WEB_WRAP_KILL_WEBS            = 52512,
@@ -30,26 +23,13 @@ enum Spells
 
 enum Events
 {
-    EVENT_WEB_SPRAY                     = 1,
-    EVENT_POISON_SHOCK                  = 2,
-    EVENT_NECROTIC_POISON               = 3,
-    EVENT_WEB_WRAP                      = 4,
-    EVENT_HEALTH_CHECK                  = 5,
-    EVENT_SUMMON_SPIDERLINGS            = 6,
+    // EVENT_WEB_SPRAY                  = 1,
+    // EVENT_POISON_SHOCK               = 2,
+    // EVENT_NECROTIC_POISON            = 3,
+    // EVENT_WEB_WRAP                   = 4,
+    // EVENT_HEALTH_CHECK               = 5,
+    // EVENT_SUMMON_SPIDERLINGS         = 6,
     EVENT_WEB_WRAP_APPLY_STUN           = 7
-};
-
-enum Emotes
-{
-    EMOTE_SPIDERS                       = 0,
-    EMOTE_WEB_WRAP                      = 1,
-    EMOTE_WEB_SPRAY                     = 2
-};
-
-enum Misc
-{
-    // NPC_WEB_WRAP                        = 16486,
-    // NPC_MAEXXNA_SPIDERLING              = 17055
 };
 
 const Position PosWrap[7] =
@@ -91,68 +71,11 @@ public:
         return GetNaxxramasAI<boss_maexxna_40AI>(pCreature);
     }
 
-    struct boss_maexxna_40AI : public BossAI
+    struct boss_maexxna_40AI : public Maexxna::boss_maexxna::boss_maexxnaAI
     {
-        explicit boss_maexxna_40AI(Creature* c) : BossAI(c, BOSS_MAEXXNA), summons(me)
-        {}
-
-        EventMap events;
-        SummonList summons;
+        explicit boss_maexxna_40AI(Creature* c) : Maexxna::boss_maexxna::boss_maexxnaAI(c) {}
 
         GuidList wraps;
-
-        bool IsInRoom()
-        {
-            if (me->GetExactDist(3486.6f, -3890.6f, 291.8f) > 100.0f)
-            {
-                EnterEvadeMode();
-                return false;
-            }
-            return true;
-        }
-
-        void Reset() override
-        {
-            BossAI::Reset();
-            events.Reset();
-            summons.DespawnAll();
-        }
-
-        void JustEngagedWith(Unit* who) override
-        {
-            BossAI::JustEngagedWith(who);
-            me->SetInCombatWithZone();
-            events.ScheduleEvent(EVENT_WEB_WRAP, 20s);
-            events.ScheduleEvent(EVENT_WEB_SPRAY, 40s);
-            events.ScheduleEvent(EVENT_POISON_SHOCK, 10s);
-            events.ScheduleEvent(EVENT_NECROTIC_POISON, 5s);
-            events.ScheduleEvent(EVENT_HEALTH_CHECK, 1s);
-            events.ScheduleEvent(EVENT_SUMMON_SPIDERLINGS, 30s);
-        }
-
-        void JustSummoned(Creature* cr) override
-        {
-            if (cr->GetEntry() == NPC_MAEXXNA_SPIDERLING)
-            {
-                cr->SetInCombatWithZone();
-                if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
-                {
-                    cr->AI()->AttackStart(target);
-                }
-            }
-            summons.Summon(cr);
-        }
-
-        void KilledUnit(Unit* who) override
-        {
-            if (who->IsPlayer())
-                instance->StorePersistentData(PERSISTENT_DATA_IMMORTAL_FAIL, 1);
-        }
-
-        void JustDied(Unit*  killer) override
-        {
-            BossAI::JustDied(killer);
-        }
 
         void DoCastWebWrap()
         {
@@ -203,69 +126,6 @@ public:
             events.ScheduleEvent(EVENT_WEB_WRAP_APPLY_STUN, 2s);
         }
 
-        void UpdateAI(uint32 diff) override
-        {
-            if (!IsInRoom())
-                return;
-
-            if (!UpdateVictim())
-                return;
-
-            events.Update(diff);
-            if (me->HasUnitState(UNIT_STATE_CASTING))
-                return;
-
-            switch (events.ExecuteEvent())
-            {
-                case EVENT_WEB_SPRAY:
-                    Talk(EMOTE_WEB_SPRAY);
-                    me->CastSpell(me, RAID_MODE(SPELL_WEB_SPRAY_10, SPELL_WEB_SPRAY_25, SPELL_WEB_SPRAY_10, SPELL_WEB_SPRAY_25), true);
-                    events.Repeat(40s);
-                    break;
-                case EVENT_POISON_SHOCK:
-                    me->CastSpell(me->GetVictim(), RAID_MODE(SPELL_POISON_SHOCK_10, SPELL_POISON_SHOCK_25, SPELL_POISON_SHOCK_10, SPELL_POISON_SHOCK_25), false);
-                    events.Repeat(10s);
-                    break;
-                case EVENT_NECROTIC_POISON:
-                    me->CastSpell(me->GetVictim(), RAID_MODE(SPELL_NECROTIC_POISON_10, SPELL_NECROTIC_POISON_25, SPELL_NECROTIC_POISON_10, SPELL_NECROTIC_POISON_25), false);
-                    events.Repeat(30s);
-                    break;
-                case EVENT_SUMMON_SPIDERLINGS:
-                    Talk(EMOTE_SPIDERS);
-                    for (uint8 i = 0; i < 8; ++i)
-                    {
-                        me->SummonCreature(NPC_MAEXXNA_SPIDERLING, me->GetPositionX(), me->GetPositionY(), me->GetPositionZ(), me->GetOrientation());
-                    }
-                    events.Repeat(40s);
-                    break;
-                case EVENT_HEALTH_CHECK:
-                    if (me->GetHealthPct() < 30)
-                    {
-                        me->CastSpell(me, RAID_MODE(SPELL_FRENZY_10, SPELL_FRENZY_25, SPELL_FRENZY_10, SPELL_FRENZY_25), true);
-                        break;
-                    }
-                    events.Repeat(1s);
-                    break;
-                case EVENT_WEB_WRAP:
-                    Talk(EMOTE_WEB_WRAP);
-                    DoCastWebWrap();
-                    events.Repeat(40s);
-                    break;
-                case EVENT_WEB_WRAP_APPLY_STUN:
-                {
-                    for (auto& p : wraps)
-                    {
-                        if (Player* player = ObjectAccessor::GetPlayer(*me, p))
-                        {
-                            player->CastSpell(player, SPELL_WEB_WRAP_STUN, true);
-                        }
-                    }
-                    wraps.clear();
-                    break;
-                }
-            }
-            DoMeleeAttackIfReady();
-        }
     };
 };
 
