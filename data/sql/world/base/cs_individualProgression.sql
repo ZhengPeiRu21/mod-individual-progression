1fd219ceaf417928b81cde423d162b0573ff965c
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-12-05 00:28:40 +0100

    Update cs_individualProgression.sql

--- data/sql/world/base/cs_individualProgression.sql
+++ data/sql/world/base/cs_individualProgression.sql
@@ -1,7 +1,7 @@
 DELETE FROM `command` WHERE `name` IN ('ip set', 'ip setbot', 'ip get', 'ip view', 'ip tele');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('ip set', 2, 'Syntax: .ip set $progressionLevel\n Sets the player to the given progression level.'),
-('ip setbot', 0, 'Syntax: .ip setbot $progressionLevel\n Sets the bot to the given progression level.'),
-('ip get', 0, 'Syntax: .ip get Shows your or your targets current progression level.'),
-('ip view', 0, 'Syntax: .ip view Shows your or your targets current progression level.'),
-('ip tele', 2, 'Syntax: .ip tele $location\n Teleports the player to the given location.');
+('ip set', 2, 'Syntax: .ip set $progressionLevel\nSets the player to the given progression level.'),
+('ip setbot', 0, 'Syntax: .ip setbot $progressionLevel\nSets the bot to the given progression level.'),
+('ip get', 0, 'Syntax: .ip get\nShows your or your targets current progression level.'),
+('ip view', 0, 'Syntax: .ip view\nShows your or your targets current progression level.'),
+('ip tele', 2, 'Syntax: .ip tele $location\nTeleports the player to the given location.');
