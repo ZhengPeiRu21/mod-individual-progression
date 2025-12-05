b12a7597f253dfdbe914864cb476a8db61689524
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-12-06 00:19:53 +0100

    Update IndividualProgression_loader.cpp

--- src/IndividualProgression_loader.cpp
+++ src/IndividualProgression_loader.cpp
@@ -48,7 +48,7 @@ void AddSC_npc_archmage_timear();
 void AddSC_karazhan_70();
 void AddSC_the_eye_70();
 void AddSC_serpentshrine_cavern_70();
-
+void AddSC_karazhan_70();
 void Addmod_individual_progressionScripts()
 {
     AddSC_mod_individual_progression();
@@ -95,5 +95,6 @@ void Addmod_individual_progressionScripts()
     AddSC_npc_archmage_timear();
     AddSC_karazhan_70();
     AddSC_the_eye_70();
+    AddSC_karazhan_70();
     AddSC_serpentshrine_cavern_70();
 }
