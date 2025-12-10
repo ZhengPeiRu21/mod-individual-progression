77a0c77a7f53fa6a244ab66dc1f50da0765ba461
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-12-10 21:27:49 +0100

    Update cs_individualProgression.sql

--- data/sql/world/base/cs_individualProgression.sql
+++ data/sql/world/base/cs_individualProgression.sql
@@ -1,5 +1,6 @@
 DELETE FROM `command` WHERE `name` IN ('individualProgression set', 'ip set', 'ip setbot', 'ip get', 'ip view', 'ip tele');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
 ('ip set', 2, 'Syntax: .ip set $progressionLevel\nSets the player to the given progression level.'),
+('ip setbot', 0, 'Syntax: .ip setbot\nSets all bots in the group to your progression level.'),
 ('ip get', 0, 'Syntax: .ip get\nShows your or your targets current progression level.'),
 ('ip tele', 2, 'Syntax: .ip tele $location\nTeleports the player to the given location.');
