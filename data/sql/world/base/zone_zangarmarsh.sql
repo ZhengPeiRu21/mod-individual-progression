91028fc21a1d3e3b7b73d957556a45c27a0ad6cf
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-12-28 22:08:36 +0100

    Create zone_zangarmarsh.sql

---
+++ data/sql/world/base/zone_zangarmarsh.sql
@@ -0,0 +1,3 @@
+DELETE FROM `creature_loot_template` WHERE `Entry` = 19402 AND `Item` = 24373;
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES 
+(19402, 24373, 0, 5, 1, 1, 0, 1, 1, 'Withered Giant - Scout Jyoba\'s Report');
