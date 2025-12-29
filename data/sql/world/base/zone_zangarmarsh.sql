de198213544b7293ba56640d2860377fb19b42b8
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-12-29 01:27:59 +0100

    Update zone_zangarmarsh.sql

--- data/sql/world/base/zone_zangarmarsh.sql
+++ data/sql/world/base/zone_zangarmarsh.sql
@@ -2,5 +2,5 @@ DELETE FROM `creature_loot_template` WHERE `Entry` = 19402 AND `Item` = 24373;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES 
 (19402, 24373, 0, 5, 1, 1, 0, 1, 1, 'Withered Giant - Scout Jyoba\'s Report');
 
--- fix position Innkeeper Coryth Stoktron so he faces Naka
-UPDATE `creature` SET `position_x` = -173.31, `position_y` = 5529.53, `orientation` = 3.46754 WHERE `id1` = 18907;
+-- fix creature placed under ground
+UPDATE `creature` SET `position_x` = -230.598, `position_y` = 6376.8, `position_z` = 19.8601 WHERE `guid` = 63967;
