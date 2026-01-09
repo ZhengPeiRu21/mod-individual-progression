2c3c54c9f40da056e88e9099404b456b6d337f44
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-01-09 08:51:33 +0100

    Update pvp_quests.sql

--- data/sql/world/base/pvp_quests.sql
+++ data/sql/world/base/pvp_quests.sql
@@ -381,7 +381,7 @@ DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` IN
 8404, 8405, 8406, 8407, 8408, 8431, 8432, 8433, 8434, 8435, 8440, 8441, 8442, 8443, 
 11335, 11336, 11337, 11338, 11339, 11340, 11341, 11342, 13475, 13476, 13477, 13478);
 
--- connect Call to Arms quests to events
+-- connect Call to Arms quests to events (doesn't work yet)
 DELETE FROM `game_event_creature_quest` WHERE `eventEntry` IN (18, 19, 20, 21) AND `quest` IN (11335, 11337, 11338, 11340);
 INSERT INTO `game_event_creature_quest` (`eventEntry`, `id`, `quest`) VALUES 
 --
@@ -393,6 +393,13 @@ INSERT INTO `game_event_creature_quest` (`eventEntry`, `id`, `quest`) VALUES
 (20, @ABG_TBC, 11335),
 (21, @HW_TBC,  11337), -- Call to Arms: Eye of the Storm
 (21, @ABG_TBC, 11337);
+
+DELETE FROM `game_event_seasonal_questrelation` WHERE `questId` IN (11335, 11337, 11338, 11340);
+INSERT INTO `game_event_seasonal_questrelation` (`questId`, `eventEntry`) VALUES 
+(11340, 18),
+(11338, 19),
+(11335, 20),
+(11337, 21);
 
 -- change unused WotLK version of 'For Great Honor' and 'Concerted Efforts' into missing vanilla version
 UPDATE `quest_template` SET `MinLevel` = 51, `RewardKillHonor` = 0,
