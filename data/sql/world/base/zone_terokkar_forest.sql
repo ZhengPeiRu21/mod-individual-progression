fbb97c99c5719f2597234b6f86655d87868bbb06
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-01-18 06:03:06 +0100

    Update zone_terokkar_forest.sql

--- data/sql/world/base/zone_terokkar_forest.sql
+++ data/sql/world/base/zone_terokkar_forest.sql
@@ -5,14 +5,15 @@ UPDATE `creature_loot_template` SET `GroupId` = 2 WHERE `Item` = 31814 AND `Entr
 UPDATE `quest_template` SET `RequiredItemId3` = 0, `RequiredItemCount3` = 0 WHERE `ID` = 10923;
 
 -- fix movement Teribus the Cursed - patrols while nobody is doing the quest.
-DELETE FROM `creature_template` WHERE `entry` IN (122441);
+DELETE FROM `creature_template` WHERE `entry` IN (22441, 122441);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, 
 `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, 
 `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, 
 `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, 
 `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
 --
-(122441, 0, 0, 0, 0, 0, 'Teribus the Cursed', NULL, NULL, 0, 65, 65, 1, 16, 0, 1, 2.28571, 1, 1, 20, 1, 1, 0, 3, 2000, 2000, 1, 1, 1, 33538, 0, 0, 0, 6, 8, 0, 0, 0, 0, 0, 0, 0, '', 1, 1, 5, 1, 1, 1, 0, 0, 1, 8388624, 0, 2, '', 12340);
+(22441,0,0,0,0,0,'Teribus the Cursed',NULL,NULL,0,65,65,1,16,0,1,2.28571,1,1,20,1,1,0,3,2000,2000,1,1,1,33536,2048,0,0,6,8,0,0,0,0,0,0,0,'SmartAI',1,1,5,1,1,1,0,0,1,8388624,0,2,'',12340),
+(122441,0,0,0,0,0,'Teribus the Cursed',NULL,NULL,0,65,65,1,16,0,1,2.28571,1,1,20,1,1,0,3,2000,2000,1,1,1,33538,0,0,0,6,8,0,0,0,0,0,0,0,'',1,1,5,1,1,1,0,0,1,8388624,0,2,'',12340);
 
 DELETE FROM `creature_template_model` WHERE `CreatureID` IN (122441);
 INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES 
@@ -44,9 +45,10 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (22441, 0, 11, 12, 11, 0, 100, 512, 0, 0, 0, 0, 0, 0, 47, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                 'Teribus the Cursed - On Respawn - Set Visible OFF'),
 (22441, 0, 12, 0, 61, 0, 100, 512, 0, 0, 0, 0, 0, 0, 47, 1, 0, 0, 0, 0, 0, 10, 622441, 122441, 0, 0, 0, 0, 0, 0,       'Teribus the Cursed - On Respawn - Set Visible ON for patrol');
 
-DELETE FROM `creature` WHERE `guid` = 622441;
+DELETE FROM `creature` WHERE `guid` IN (247237, 622441);
 INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, 
 `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
+(247237, 22441, 0, 0, 530, 0, 0, 1, 1, 0, -3400.86, 4665.3, 99.6544, 4.62677, 300, 0, 0, 29570, 0, 0, 0, 0, 0, '', 0, 0, 'SAI Target'),
 (622441, 122441, 0, 0, 530, 0, 0, 1, 1, 0, -3536.9500, 4552.8501, 83.9206, 1.4071, 300, 0, 1, 29570, 0, 2, 0, 0, 0, '', 0, 0, 'Patrol');
 
 DELETE FROM `creature_addon` WHERE `guid` = 622441;
