7e258d031d50ec05191f7fa1ef0a54470a0ba07f
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-01-20 00:07:12 +0100

    Update pvp_quests.sql

--- data/sql/world/base/pvp_quests.sql
+++ data/sql/world/base/pvp_quests.sql
@@ -376,6 +376,8 @@ UPDATE `quest_template` SET `RewardItem1` = 20558, `RewardAmount1` = 3 WHERE `ID
 UPDATE `quest_template_addon` SET `PrevQuestID` = 13476, `SpecialFlags` = 1 WHERE `ID` = 13475;
 UPDATE `quest_template_addon` SET `PrevQuestID` = 13478, `SpecialFlags` = 1 WHERE `ID` = 13477;
 
+UPDATE `quest_template_addon` SET `SpecialFlags` = 0 WHERE `ID` IN (8369, 8375); -- set AV quests to not repeatable
+
 DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` IN 
 (8367, 8371, 8384, 8385, 8386, 8388, 8389, 8390, 8391, 8392, 8397, 8398, 
 8404, 8405, 8406, 8407, 8408, 8431, 8432, 8433, 8434, 8435, 8440, 8441, 8442, 8443, 
