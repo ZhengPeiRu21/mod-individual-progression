90b3efe0e50bf8f05744eea57afb7a76b2db2cc3
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-01-23 08:10:09 +0100

    Update dungeon_karazhan.sql

--- data/sql/world/base/dungeon_karazhan.sql
+++ data/sql/world/base/dungeon_karazhan.sql
@@ -12,7 +12,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (15551, 0, 2, 0, 1, 0, 60, 0, 0, 70000, 80000, 190000, 0, 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,          'Spectral Stable Hand - Out of Combat - Say Line 2'),
 (15551, 0, 3, 0, 0, 0, 100, 0, 2000, 11000, 12000, 21000, 0, 0, 11, 18812, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,  'Spectral Stable Hand - In Combat - Cast Knockdown'),
 (15551, 0, 4, 0, 0, 0, 100, 0, 2000, 15000, 17000, 28000, 0, 0, 11, 6016, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,   'Spectral Stable Hand - In Combat - Cast Pierce Armor'),
-(15551, 0, 5, 0, 74, 0, 100, 0, 0, 0, 14000, 22000, 50, 40, 11, 29339, 0, 0, 0, 0, 0, 9, 15547, 0, 40, 1, 0, 0, 0, 0, 'Spectral Stable Hand - Spectral Charger below 50% hp - Cast Healing Touch'),
+(15551, 0, 5, 0, 74, 0, 100, 0, 0, 0, 14000, 22000, 70, 40, 11, 29339, 0, 0, 0, 0, 0, 9, 15547, 0, 40, 1, 0, 0, 0, 0, 'Spectral Stable Hand - Spectral Charger below 50% hp - Cast Healing Touch'),
 (15551, 0, 6, 0, 0, 0, 100, 0, 2000, 15000, 21000, 38000, 0, 0, 11, 29340, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,  'Spectral Stable Hand - In Combat - Cast Whip Rage');
 
 -- Restore Enchanting formula drops to their pre-3.1 rates
