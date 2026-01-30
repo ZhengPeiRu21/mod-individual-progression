f1a00f953fbaf20cb6ea0bc8c808f867c0522998
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-01-30 12:27:28 +0100

    Create zone_bloodmyst_isle.sql

---
+++ data/sql/world/base/zone_bloodmyst_isle.sql
@@ -0,0 +1,3 @@
+-- fix Lord Xiz path ID 0 error
+UPDATE `creature` SET `MovementType` = 2`, currentwaypoint` = 1 WHERE `id1` = 17701;
+UPDATE `creature_template` SET `MovementType` = 0 WHERE `entry` = 17701
