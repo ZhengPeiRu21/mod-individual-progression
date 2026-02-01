f916a2b167aa772659cec420dce2b7eb50dd8240
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-02-01 12:02:40 +0100

    Update 00_cleanup.sql

--- data/sql/world/base/00_cleanup.sql
+++ data/sql/world/base/00_cleanup.sql
@@ -30,7 +30,7 @@ DELETE FROM `creature` WHERE `guid` = 88156 AND `id1` IN (20278); -- Vixton Pinc
 /* the following edits are temporary */
 
 -- no longer hide npc, now hiding vendor items instead
-UPDATE `creature_template` SET `ScriptName` = '' WHERE `entry` IN (28701);
+UPDATE `creature_template` SET `ScriptName` = '' WHERE `entry` IN (28701, 32172);
 
 -- remove unused riding spell list
 DELETE FROM `trainer_spell` WHERE `TrainerId` = 648;
