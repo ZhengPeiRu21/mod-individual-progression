2aaed8261a2acd5d2749649b5a4b029ba0792b86
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-02-01 06:44:33 +0100

    Update zone_thunder_bluff.sql

--- data/sql/world/base/zone_thunder_bluff.sql
+++ data/sql/world/base/zone_thunder_bluff.sql
@@ -16,41 +16,6 @@ UPDATE `creature_template` SET `type_flags` = 134217728 WHERE `entry` IN (3004, 
 /* NPC Thrumn - Remove non-Vanilla Tabards */
 DELETE FROM `npc_vendor` WHERE `entry`= 5189 AND `item` IN (15197, 15199, 19031, 19505, 24004, 31773, 31775, 31776, 31777, 31778, 31779, 31780, 31781, 31804, 32445, 32828, 35221);
 
-SET @TRAINER_ID   := 600;
-
-DELETE FROM `creature_default_trainer` WHERE `CreatureId` IN (2798, 2998, 3001, 3004, 3007, 3008, 3009, 3011, 3013, 3026, 3028, 7089, 10278, 11047, 11051, 11071, 11084);
-INSERT INTO `creature_default_trainer` (`CreatureId`, `TrainerId`) VALUES
-(2798,  @TRAINER_ID+45), -- Pand Stonebinder <First Aid Trainer>
-(2998,  @TRAINER_ID+17), -- Karn Stonehoof <Expert Blacksmith>
-(3001,  @TRAINER_ID+42), -- Brek Stonehoof <Mining Trainer>
-(3004,  @TRAINER_ID+37), -- Tepa <Expert Tailor>
-(3007,  @TRAINER_ID+33), -- Una <Artisan Leatherworker>
-(3008,  @TRAINER_ID+31), -- Mak <Journeyman Leatherworker>
-(3009,  @TRAINER_ID+12), -- Bena Winterhoof <Expert Alchemist>
-(3011,  @TRAINER_ID+22), -- Teg Dawnstrider <Expert Enchanter>
-(3013,  @TRAINER_ID+41), -- Komin Winterhoof <Herbalism Trainer>
-(3026,  @TRAINER_ID+44), -- Aska Mistrunner <Cooking Trainer>
-(3028,  @TRAINER_ID+47), -- Kah Mistrunner <Fishing Trainer>
-(7089,  @TRAINER_ID+43), -- Mooranta <Skinning Trainer>
-(10278, @TRAINER_ID+16), -- Thrag Stonehoof <Journeyman Blacksmith>
-(11047, @TRAINER_ID+11), -- Kray <Journeyman Alchemist>
-(11051, @TRAINER_ID+36), -- Vhan <Journeyman Tailor>
-(11071, @TRAINER_ID+21), -- Mot Dawnstrider <Journeyman Enchanter>
-(11084, @TRAINER_ID+32); -- Tarn <Expert Leatherworker>
-
-DELETE FROM `gossip_menu_option` WHERE `MenuID` IN (2748, 4119, 4128, 4158, 4165, 4187, 4207, 4242, 4266, 4351); 
-INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`) VALUES
-(2748, 0, 3, 'Train me.', 3266, 5, 16), -- Thrag Stonehoof <Journeyman Blacksmith>
-(4119, 0, 3, 'Train me.', 3266, 5, 16), -- Kray <Journeyman Alchemist>
-(4128, 0, 3, 'Train me.', 3266, 5, 16), -- Bena Winterhoof <Expert Alchemist>
-(4158, 0, 3, 'Train me.', 3266, 5, 16), -- Mot Dawnstrider <Journeyman Enchanter>
-(4165, 0, 3, 'Train me.', 3266, 5, 16), -- Teg Dawnstrider <Expert Enchanter>
-(4187, 0, 3, 'Train me.', 3266, 5, 16), -- Mak <Journeyman Leatherworker>
-(4207, 0, 3, 'Train me.', 3266, 5, 16), -- Tarn <Expert Leatherworker>
-(4242, 0, 3, 'Train me.', 3266, 5, 16), -- Una <Artisan Leatherworker>
-(4266, 0, 3, 'Train me.', 3266, 5, 16), -- Vhan <Journeyman Tailor>
-(4351, 0, 3, 'Train me.', 3266, 5, 16); -- Tepa <Expert Tailor>
-
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceEntry` = 0 AND `ConditionTypeOrReference` = 7 AND `SourceGroup` IN (1017, 4128, 4165, 4207, 4242, 4351);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`, `Comment`) VALUES
 (15, 1017, 0, 7, 164, 50,  'Show menu if blacksmithing is 50 or higher'),   -- Karn Stonehoof <Expert Blacksmith>
