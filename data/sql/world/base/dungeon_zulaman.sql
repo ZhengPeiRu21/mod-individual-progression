e27d4584c1fa871cecbbb72844e0c9f7b5ea243e
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-02-03 07:53:04 +0100

    Update dungeon_zulaman.sql

--- data/sql/world/base/dungeon_zulaman.sql
+++ data/sql/world/base/dungeon_zulaman.sql
@@ -1,3 +1,18 @@
+/* smart scripts */
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN 
+(24043);
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` IN 
+(24043);
+
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, 
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, 
+`action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
+`target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+--
+(24043, 0, 0, 0, 1, 0, 100, 1, 1000, 1000, 0, 0, 0, 0, 11, 42943, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,         'Amani Lynx Cub - Out of Combat - Cast \'Stealth\''),
+(24043, 0, 1, 0, 4, 0, 100, 512, 0, 0, 0, 0, 0, 0, 28, 42943, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,             'Amani Lynx Cub - On Aggro - Remove Aura \'Stealth\''),
+(24043, 0, 2, 0, 0, 0, 100, 0, 5000, 5000, 35000, 35000, 0, 0, 11, 43357, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Amani Lynx Cub - In Combat - Cast \'Feral Swipe\'');
+
 SET @REFIDTHREE = 35102,
 @REFIDFOUR      = 527000,
 @TANZAR         = 22699,
