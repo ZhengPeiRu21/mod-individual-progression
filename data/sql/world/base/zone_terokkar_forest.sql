2eb767a1367e52e19d19c8d8a5d061dfafeefbff
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-03-18 03:33:22 +0100

    Update zone_terokkar_forest.sql
    setting 2 additional unit_flags solves the evade issue.

--- data/sql/world/base/zone_terokkar_forest.sql
+++ data/sql/world/base/zone_terokkar_forest.sql
@@ -48,7 +48,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (22441, 0, 11, 12, 11, 0, 100, 512, 0, 0, 0, 0, 0, 0, 47, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                 'Teribus the Cursed - On Respawn - Set Visible OFF'),
 (22441, 0, 12, 0, 61, 0, 100, 512, 0, 0, 0, 0, 0, 0, 47, 1, 0, 0, 0, 0, 0, 10, 622441, 122441, 0, 0, 0, 0, 0, 0,       'Teribus the Cursed - On Respawn - Set Visible ON for patrol');
 
--- fix Terokk evade issue
+-- fix Terokk evade issue -- 00_cleanup (this can be removed at some point)
 DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` = 21838;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (21838, 0, 0, 0, 0, 0, 100, 0, 4000, 7000, 10000, 15000, 0, 0, 11, 40721, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,     'Terokk - In Combat - Cast \'Shadow Bolt Volley\''),
@@ -61,9 +61,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (21838, 0, 7, 8, 32, 0, 100, 0, 0, 1000000, 0, 0, 0, 0, 28, 40733, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,            'Terokk - On Damaged by \'Ancient Flames\' - Remove Aura \'Divine Shield\' (Phase 1)'), -- Only with Divine Shield
 (21838, 0, 8, 9, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                       'Terokk - On Spellhit \'Ancient Flames\' - Say Line 2'),
 (21838, 0, 9, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 28747, 34, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                 'Terokk - On Spellhit \'Ancient Flames\' - Cast \'Frenzy\''),
--- (21838, 0, 10, 0, 7, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 19, 23377, 90, 0, 0, 0, 0, 0, 0,             'Terokk - On Evade - Despawn Target'),
-(21838, 0, 10, 0, 7, 0, 100, 0, 0, 0, 0, 0, 0, 0, 223, 2, 0, 0, 0, 0, 0, 10, 12478, 23377, 0, 0, 0, 0, 0, 0,            'Terokk - On Evade - Do Action on Skyguard Ace Retreat'),
-(21838, 0, 11, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 223, 1, 0, 0, 0, 0, 0, 10, 12478, 23377, 0, 0, 0, 0, 0, 0,            'Terokk - On Just Died - Do Action Terokk Dead');
+(21838, 0, 10, 11, 7, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 19, 23377, 90, 0, 0, 0, 0, 0, 0,               'Terokk - On Evade - Despawn Target'),
+(21838, 0, 11, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                     'Terokk - On Evade - Despawn Instant'),
+(21838, 0, 12, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 223, 1, 0, 0, 0, 0, 0, 10, 12478, 23377, 0, 0, 0, 0, 0, 0,            'Terokk - On Just Died - Do Action Terokk Dead');
+
+-- add UNIT_FLAG_IMMUNE_TO_PC and UNIT_FLAG_IMMUNE_TO_NPC flags to avoid evade
+UPDATE `creature_template` SET `unit_flags` = 33600 WHERE `entry` = 21838;
 
 -- fix SMART_ACTION_TALK worldserver errors
 UPDATE `smart_scripts` SET `target_type` = 21, `target_param1` = 40 WHERE `entryorguid` = 1410 AND `source_type` = 0 AND `id` = 3;
