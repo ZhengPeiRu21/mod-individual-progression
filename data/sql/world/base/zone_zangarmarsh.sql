87d10b25a6e3115137065785a15a4133c13f85de
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-03-21 05:24:43 +0100

    Update zone_zangarmarsh.sql

--- data/sql/world/base/zone_zangarmarsh.sql
+++ data/sql/world/base/zone_zangarmarsh.sql
@@ -3,3 +3,10 @@ UPDATE `creature_loot_template` SET `Chance` = 20 WHERE `Item` = 24373 AND `Entr
 
 -- fix creature placed under ground
 UPDATE `creature` SET `position_x` = -230.598, `position_y` = 6376.8, `position_z` = 19.8601 WHERE `guid` = 63967;
+
+-- Juno Dufrain <Fishing Supplies>
+DELETE FROM `gossip_menu_option` WHERE `MenuID` IN (10437);
+INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, 
+`OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES 
+(10437, 0, 1, 'Let me browse your goods.', 8097, 3, 128, 0, 0, 0, 0, '', 0, 0),
+(10437, 1, 3, 'I seek training in Fishing.', 34245, 5, 16, 0, 0, 0, 0, '', 0, 0);
