9276e67e35cac0e42fbb4e50a1670d4db21f890b
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-04-28 12:20:20 +0200

    Update IndividualProgression.h

--- src/IndividualProgression.h
+++ src/IndividualProgression.h
@@ -422,6 +422,7 @@ public:
     void checkIPProgression(Player* player);
     void UpdateProgressionAchievements(Player* player, uint16 achievementID);
     void checkKillProgression(Player* player, Creature* killed);
+	void UpdateAccountReputation(uint32 factionId, uint32 accountId, Player* player);
     void CleanUpVanillaPvpTitles(Player* player);
     void AwardEarnedVanillaPvpTitles(Player* player);
     static void LoadCustomProgressionEntries(const std::string& customProgressionString);
