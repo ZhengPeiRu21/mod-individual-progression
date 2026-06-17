16ac39d90ce378d15a6a3b1f94072caff41e4dea
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-06-17 23:39:16 +0200

    Update cs_individualProgression.sql

--- data/sql/world/base/cs_individualProgression.sql
+++ data/sql/world/base/cs_individualProgression.sql
@@ -1,8 +1,9 @@
-DELETE FROM `command` WHERE `name` IN ('ip get', 'ip set', 'ip setbot', 'ip setrep', 'ip tele', 'ip pvp');
+DELETE FROM `command` WHERE `name` IN ('ip get', 'ip set', 'ip setbot', 'ip setrep', 'ip tele', 'ip pvp', 'ip attune');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
 ('ip get', 0, 'Syntax: .ip get [$player]\nShows the current progression level for yourself, your target, or a named player.'),
 ('ip set', 2, 'Syntax: .ip set $progressionLevel\nSets the player to the given progression level.'),
 ('ip setbot', 0, 'Syntax: .ip setbot\nSets all bots in the group to your progression level.'),
 ('ip setrep', 0, 'Syntax: .ip setrep\nSets your reputation of certain factions to the same value as the character that has the highest value on your account.'),
 ('ip tele', 2, 'Syntax: .ip tele $location\nTeleports the player to the given location.'),
+('ip attune', 0, 'Syntax: .ip tele $location\nGives all players in the group the required attument item.'),
 ('ip pvp', 0, 'Syntax: .ip pvp [$player]\nShows the current PvP rank and kills for yourself, your target, or a named player.');
