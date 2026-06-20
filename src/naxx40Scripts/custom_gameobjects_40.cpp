d365bf0e368826a593b8a295b4f737915d360c5e
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-06-20 16:53:35 +0200

    Update custom_gameobjects_40.cpp

--- src/naxx40Scripts/custom_gameobjects_40.cpp
+++ src/naxx40Scripts/custom_gameobjects_40.cpp
@@ -25,38 +25,103 @@ public:
 
     bool OnGossipHello(Player* player, GameObject* /*go*/) override
     {
-        if (player->GetLevel() > 70)
-        {
-            ChatHandler(player->GetSession()).PSendSysMessage("Your level is too high to enter the level 60 version of Naxxramas.");
+        if (!player || !player->IsInWorld())
             return false;
-        }
 
-        if (sIndividualProgression->hasPassedProgression(player, PROGRESSION_TBC_TIER_5))
-        {
-            ChatHandler(player->GetSession()).PSendSysMessage("Your progression level is too high to enter the level 60 version of Naxxramas.");
-            return false;
-        }
+        ChatHandler handler(player->GetSession());
+        Group* group = player->GetGroup();
 
-        if (!sIndividualProgression->requireNaxxStrath || player->GetQuestStatus(NAXX40_ENTRANCE_FLAG) == QUEST_STATUS_REWARDED)
+        if (player->GetLevel() <= 70)
         {
-            if (sIndividualProgression->isAttuned(player))
+            bool allowed = true;
+
+            if (sIndividualProgression->hasPassedProgression(player, PROGRESSION_TBC_TIER_5)) // death knights
             {
-                Group* group = player->GetGroup();
+                handler.PSendSysMessage("Your progression level is too high.");
+                allowed = false;
+            }
 
-                if (group)
-                    group->SetRaidDifficulty(RAID_DIFFICULTY_10MAN_HEROIC);
+            if (sIndividualProgression->requireNaxxStrath)
+            {
+                if (player->GetQuestStatus(NAXX40_ENTRANCE_FLAG) == QUEST_STATUS_COMPLETE) {}
                 else
-                    player->SetRaidDifficulty(RAID_DIFFICULTY_10MAN_HEROIC);
+                {
+                    handler.PSendSysMessage("You need to enter through Stratholme first. (RequireNaxxStrathEntrance is enabled)");
+                    allowed = false;
+                }
+            }
 
-                player->TeleportTo(533, 3005.51f, -3434.64f, 304.195f, 6.2831f);
-                return true;
-            }
-            else
+            if (!sIndividualProgression->isAttuned(player))
             {
-                ChatHandler(player->GetSession()).PSendSysMessage("You have not completed the Naxxramas attunement quest.");
-                return false;
+                handler.PSendSysMessage("You are not attuned to Naxxramas.");
+                return allowed = false;
             }
+
+            if (!allowed)
+                return false;
+
+            if (group)
+            {
+                group->SetRaidDifficulty(RAID_DIFFICULTY_10MAN_HEROIC);
+
+                for (GroupReference* itr = group->GetFirstMember(); itr; itr = itr->next())
+                {
+                    Player* member = itr->GetSource();
+                    if (!member || sIndividualProgression->isBotAccount(member))
+                        continue;
+
+                    if (member->GetGUID() == player->GetGUID()) // not checking the player who is using the teleporter again
+                        continue;
+
+                    bool allowed = true;
+
+                    if (sIndividualProgression->requireNaxxStrath)
+                    {
+                        if (member->GetQuestStatus(NAXX40_ENTRANCE_FLAG) == QUEST_STATUS_COMPLETE) {}
+                        else
+                        {
+                            handler.PSendSysMessage("|cff00ffff{}|r needs to enter through Stratholme first. (RequireNaxxStrathEntrance is enabled)", member->GetName());
+                            allowed = false;
+                        }
+                    }
+
+                    if (sIndividualProgression->hasPassedProgression(member, PROGRESSION_TBC_TIER_5)) // death knights
+                    {
+                        handler.PSendSysMessage("|cff00ffff{}|r progression level is too high.", member->GetName());
+                        allowed = false;
+                    }
+
+                    if (!sIndividualProgression->isAttuned(member))
+                    {
+                        handler.PSendSysMessage("|cff00ffff{}|r is not attuned to Naxxramas.", member->GetName());
+                        allowed = false;
+                    }
+
+                    if (member->IsGameMaster())
+                    {
+                        handler.PSendSysMessage("|cff00ffff{}|r is a GM.", member->GetName());
+                        allowed = true;
+                    }
+
+                    if (allowed)
+                    {
+                        handler.PSendSysMessage("|cff00ffff{}|r is allowed to enter.", member->GetName());
+
+                        member->SetRaidDifficulty(RAID_DIFFICULTY_10MAN_HEROIC);
+                        member->TeleportTo(533, 3005.51f, -3434.64f, 304.195f, 6.2831f);
+                    }
+                }
+            }
+
+            player->SetRaidDifficulty(RAID_DIFFICULTY_10MAN_HEROIC);
+            player->TeleportTo(533, 3005.51f, -3434.64f, 304.195f, 6.2831f);
+            return true;
         }
+        else
+        {
+            handler.PSendSysMessage("You need to be level 70 or below to enter this version of Naxxramas.");
+        }
+
         return false;
     }
 };
