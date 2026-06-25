612fce02c62b55af78621ea6c62c11ca3cbb2df7
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-06-25 19:25:01 +0200

    Update IndividualProgression.cpp

--- src/IndividualProgression.cpp
+++ src/IndividualProgression.cpp
@@ -161,12 +161,12 @@ void IndividualProgression::UpdateRNDbotSpells(Player* player)
     switch (player->getClass())
     {
     case CLASS_WARLOCK:
-        if (player->GetLevel() >= 40 && !player->HasSpell(1710)) // Summon Felsteed
-            player->learnSpell(1710, false);
+        if (player->GetLevel() >= 40 && !player->HasSpell(7584)) // Summon Felsteed
+            player->learnSpell(7584, false);
         break;
     case CLASS_PALADIN:
-        if (player->GetLevel() >= 40 && !player->HasSpell(13820)) // Summon Warhorse
-            player->learnSpell(13820, false);
+        if (player->GetLevel() >= 40 && !player->HasSpell(13819)) // Summon Warhorse
+            player->learnSpell(13819, false);
         break;
     case CLASS_DRUID:
         if (player->GetLevel() >= 10 && !player->HasSpell(18960)) // Teleport: Moonglade
