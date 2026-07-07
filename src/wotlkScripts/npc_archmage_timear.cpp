85898c90a09e05d62b86103cc3a9808b3c0167dd
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-07-07 22:53:29 +0200

    Update npc_archmage_timear.cpp

--- src/wotlkScripts/npc_archmage_timear.cpp
+++ src/wotlkScripts/npc_archmage_timear.cpp
@@ -4,7 +4,8 @@
 #include "ScriptedGossip.h"
 #include "IndividualProgression.h"
 
-enum ArchmageTimearQuests
+// Archmage Timear offered daily quests during WotLK until patch 3.3. After that Archmage Landalock handled the daily quests until patch 3.4.
+enum ArchmageTimearDailyQuests
 {
     QUEST_PROOF_OF_DEMISE_INGVAR_THE_PLUNDERER = 13245,
     QUEST_PROOF_OF_DEMISE_KERISTRASZA = 13246,
@@ -17,10 +18,11 @@ enum ArchmageTimearQuests
     QUEST_PROOF_OF_DEMISE_LOKEN = 13253,
     QUEST_PROOF_OF_DEMISE_ANUB_ARAK = 13254,
     QUEST_PROOF_OF_DEMISE_HERALD_VOLAZJ = 13255,
-    QUEST_PROOF_OF_DEMISE_CYANIGOSA = 13256
+    QUEST_PROOF_OF_DEMISE_CYANIGOSA = 13256,
+    QUEST_PROOF_OF_DEMISE_THE_BLACK_KNIGHT = 14199
 };
 
-enum ArchmageTimearImages
+enum ArchmageTimearDailyImages
 {
     NPC_INGVAR_THE_PLUNDERER_IMAGE = 31584,
     NPC_KERISTRASZA_IMAGE = 31618,
@@ -33,15 +35,14 @@ enum ArchmageTimearImages
     NPC_LOKEN_IMAGE = 31625,
     NPC_ANUB_ARAK_IMAGE = 31626,
     NPC_HERALD_VOLAZJ_IMAGE = 31627,
-    NPC_CYANIGOSA_IMAGE = 31629
+    NPC_CYANIGOSA_IMAGE = 31629,
+    NPC_THE_BLACK_KNIGHT_IMAGE = 35461
 };
 
 class npc_archmage_timear : public CreatureScript
 {
 public:
-    npc_archmage_timear() : CreatureScript("npc_archmage_timear")
-    {
-    }
+    npc_archmage_timear() : CreatureScript("npc_archmage_timear") { }
 
     CreatureAI* GetAI(Creature* creature) const override
     {
@@ -54,6 +55,15 @@ public:
         {
             _switchImageTimer = MINUTE * IN_MILLISECONDS;
             _summonGUID.Clear();
+        }
+
+        bool CanBeSeen(Player const* player) override
+        {
+            if (player->IsGameMaster())
+                return true;
+
+            Player* target = ObjectAccessor::FindConnectedPlayer(player->GetGUID());
+            return sIndividualProgression->isBeforeProgression(target, PROGRESSION_WOTLK_TIER_2);
         }
 
         uint32 GetImageEntry(uint32 QuestId)
@@ -131,7 +141,8 @@ public:
     };
 };
 
-enum ArchmageLandalockQuests
+// After WotLK patch 3.4 Archmange Landalock ONLY offered weekly quests. The weekly quests replaced the daily quests.
+enum ArchmageLandalockWeeklyQuests
 {
     QUEST_SARTHARION_MUST_DIE               = 24579,
     QUEST_ANUBREKHAN_MUST_DIE               = 24580,
@@ -145,10 +156,9 @@ enum ArchmageLandalockQuests
     QUEST_XT_002_DECONSTRUCTOR_MUST_DIE     = 24588,
     QUEST_LORD_JARAXXUS_MUST_DIE            = 24589,
     QUEST_LORD_MARROWGAR_MUST_DIE           = 24590,
-    QUEST_PROOF_OF_DEMISE_THE_BLACK_KNIGHT  = 14199
 };
 
-enum ArchmageLandalockImages
+enum ArchmageLandalockWeeklyImages
 {
     NPC_SARTHARION_IMAGE                = 37849,
     NPC_ANUBREKHAN_IMAGE                = 37850,
@@ -161,25 +171,128 @@ enum ArchmageLandalockImages
     NPC_IGNIS_THE_FURNACE_MASTER_IMAGE  = 37859,
     NPC_XT_002_DECONSTRUCTOR_IMAGE      = 37861,
     NPC_LORD_JARAXXUS_IMAGE             = 37862,
-    NPC_LORD_MARROWGAR_IMAGE            = 37864,
-    NPC_THE_BLACK_KNIGHT_IMAGE          = 35461
+    NPC_LORD_MARROWGAR_IMAGE            = 37864
 };
 
-class npc_archmage_landalock_ipp : public CreatureScript
+class npc_archmage_landalock_ipp_3_3 : public CreatureScript
 {
 public:
-    npc_archmage_landalock_ipp() : CreatureScript("npc_archmage_landalock")
-    {
-    }
+    npc_archmage_landalock_ipp_3_3() : CreatureScript("npc_archmage_landalock_3_3") { }
 
     CreatureAI* GetAI(Creature* creature) const override
     {
-        return new npc_archmage_landalock_ippAI(creature);
+        return new npc_archmage_landalockAI(creature);
     }
 
-    struct npc_archmage_landalock_ippAI : public ScriptedAI
+    struct npc_archmage_landalockAI : public ScriptedAI
     {
-        npc_archmage_landalock_ippAI(Creature* creature) : ScriptedAI(creature)
+        npc_archmage_landalockAI(Creature* creature) : ScriptedAI(creature)
+        {
+            _switchImageTimer = MINUTE * IN_MILLISECONDS;
+            _summonGUID.Clear();
+        }
+
+        bool CanBeSeen(Player const* player) override
+        {
+            if (player->IsGameMaster())
+                return true;
+
+            Player* target = ObjectAccessor::FindConnectedPlayer(player->GetGUID());
+            return (sIndividualProgression->hasPassedProgression(target, PROGRESSION_WOTLK_TIER_2) && sIndividualProgression->isBeforeProgression(target, PROGRESSION_WOTLK_TIER_3));
+        }
+
+        uint32 GetDailyImageEntry(uint32 QuestId)
+        {
+            switch (QuestId)
+            {
+            case QUEST_SARTHARION_MUST_DIE:
+                return NPC_SARTHARION_IMAGE;
+            case QUEST_ANUBREKHAN_MUST_DIE:
+                return NPC_ANUBREKHAN_IMAGE;
+            case QUEST_NOTH_THE_PLAGUEBINGER_MUST_DIE:
+                return NPC_NOTH_THE_PLAGUEBINGER_IMAGE;
+            case QUEST_INSTRUCTOR_RAZUVIOUS_MUST_DIE:
+                return NPC_INSTRUCTOR_RAZUVIOUS_IMAGE;
+            case QUEST_PATCHWERK_MUST_DIE:
+                return NPC_PATCHWERK_IMAGE;
+            case QUEST_MALYGOS_MUST_DIE:
+                return NPC_MALYGOS_IMAGE;
+            case QUEST_FLAME_LEVIATHAN_MUST_DIE:
+                return NPC_FLAME_LEVIATHAN_IMAGE;
+            case QUEST_RAZORSCALE_MUST_DIE:
+                return NPC_RAZORSCALE_IMAGE;
+            case QUEST_IGNIS_THE_FURNACE_MASTER_MUST_DIE:
+                return NPC_IGNIS_THE_FURNACE_MASTER_IMAGE;
+            case QUEST_XT_002_DECONSTRUCTOR_MUST_DIE:
+                return NPC_XT_002_DECONSTRUCTOR_IMAGE;
+            case QUEST_LORD_JARAXXUS_MUST_DIE:
+                return NPC_LORD_JARAXXUS_IMAGE;
+            case QUEST_LORD_MARROWGAR_MUST_DIE:
+                return NPC_LORD_MARROWGAR_IMAGE;
+            default: // QUEST_PROOF_OF_DEMISE_THE_BLACK_KNIGHT:
+                return NPC_THE_BLACK_KNIGHT_IMAGE;
+            }
+        }
+
+        void JustSummoned(Creature* image) override
+        {
+            if (image->GetEntry() != NPC_ANUBREKHAN_IMAGE)
+                image->SetUnitMovementFlags(MOVEMENTFLAG_RIGHT);
+            _summonGUID = image->GetGUID();
+        }
+
+
+        void UpdateAI(uint32 diff) override
+        {
+            ScriptedAI::UpdateAI(diff);
+
+            _switchImageTimer += diff;
+            if (_switchImageTimer > MINUTE * IN_MILLISECONDS)
+            {
+                _switchImageTimer = 0;
+                QuestRelationBounds objectQR = sObjectMgr->GetCreatureQuestRelationBounds(me->GetEntry());
+
+                for (QuestRelations::const_iterator i = objectQR.first; i != objectQR.second; ++i)
+                {
+                    uint32 questId = i->second;
+                    Quest const* quest = sObjectMgr->GetQuestTemplate(questId);
+
+                    if (!quest || !quest->IsDaily())
+                        continue;
+
+                    uint32 newEntry = GetDailyImageEntry(questId);
+                    if (_summonGUID.GetEntry() != newEntry)
+                    {
+                        if (Creature* image = ObjectAccessor::GetCreature(*me, _summonGUID))
+                            image->DespawnOrUnsummon();
+
+                        float z = 653.622f;
+                        if (newEntry == NPC_KERISTRASZA_IMAGE || newEntry == NPC_CYANIGOSA_IMAGE)
+                            z += 3.0f;
+                        me->SummonCreature(newEntry, 5703.077f, 583.9757f, z, 3.926991f);
+                    }
+                }
+            }
+        }
+    private:
+        uint32 _switchImageTimer;
+        ObjectGuid _summonGUID;
+    };
+};
+
+class npc_archmage_landalock_ipp_3_4 : public CreatureScript
+{
+public:
+    npc_archmage_landalock_ipp_3_4() : CreatureScript("npc_archmage_landalock_3_4") {}
+
+    CreatureAI* GetAI(Creature* creature) const override
+    {
+        return new npc_archmage_landalockAI(creature);
+    }
+
+    struct npc_archmage_landalockAI : public ScriptedAI
+    {
+        npc_archmage_landalockAI(Creature* creature) : ScriptedAI(creature)
         {
             _switchImageTimer = MINUTE * IN_MILLISECONDS;
             _summonGUID.Clear();
@@ -194,46 +307,44 @@ public:
             return sIndividualProgression->hasPassedProgression(target, PROGRESSION_WOTLK_TIER_3);
         }
 
-        uint32 GetImageEntry(uint32 QuestId)
+        uint32 GetWeeklyImageEntry(uint32 QuestId)
         {
             switch (QuestId)
             {
-                case QUEST_SARTHARION_MUST_DIE:
-                    return NPC_SARTHARION_IMAGE;
-                case QUEST_ANUBREKHAN_MUST_DIE:
-                    return NPC_ANUBREKHAN_IMAGE;
-                case QUEST_NOTH_THE_PLAGUEBINGER_MUST_DIE:
-                    return NPC_NOTH_THE_PLAGUEBINGER_IMAGE;
-                case QUEST_INSTRUCTOR_RAZUVIOUS_MUST_DIE:
-                    return NPC_INSTRUCTOR_RAZUVIOUS_IMAGE;
-                case QUEST_PATCHWERK_MUST_DIE:
-                    return NPC_PATCHWERK_IMAGE;
-                case QUEST_MALYGOS_MUST_DIE:
-                    return NPC_MALYGOS_IMAGE;
-                case QUEST_FLAME_LEVIATHAN_MUST_DIE:
-                    return NPC_FLAME_LEVIATHAN_IMAGE;
-                case QUEST_RAZORSCALE_MUST_DIE:
-                    return NPC_RAZORSCALE_IMAGE;
-                case QUEST_IGNIS_THE_FURNACE_MASTER_MUST_DIE:
-                    return NPC_IGNIS_THE_FURNACE_MASTER_IMAGE;
-                case QUEST_XT_002_DECONSTRUCTOR_MUST_DIE:
-                    return NPC_XT_002_DECONSTRUCTOR_IMAGE;
-                case QUEST_LORD_JARAXXUS_MUST_DIE:
-                    return NPC_LORD_JARAXXUS_IMAGE;
-                case QUEST_LORD_MARROWGAR_MUST_DIE:
-                    return NPC_LORD_MARROWGAR_IMAGE;
-                default: // QUEST_PROOF_OF_DEMISE_THE_BLACK_KNIGHT:
-                    return NPC_THE_BLACK_KNIGHT_IMAGE;
+            case QUEST_SARTHARION_MUST_DIE:
+                return NPC_SARTHARION_IMAGE;
+            case QUEST_ANUBREKHAN_MUST_DIE:
+                return NPC_ANUBREKHAN_IMAGE;
+            case QUEST_NOTH_THE_PLAGUEBINGER_MUST_DIE:
+                return NPC_NOTH_THE_PLAGUEBINGER_IMAGE;
+            case QUEST_INSTRUCTOR_RAZUVIOUS_MUST_DIE:
+                return NPC_INSTRUCTOR_RAZUVIOUS_IMAGE;
+            case QUEST_PATCHWERK_MUST_DIE:
+                return NPC_PATCHWERK_IMAGE;
+            case QUEST_MALYGOS_MUST_DIE:
+                return NPC_MALYGOS_IMAGE;
+            case QUEST_FLAME_LEVIATHAN_MUST_DIE:
+                return NPC_FLAME_LEVIATHAN_IMAGE;
+            case QUEST_RAZORSCALE_MUST_DIE:
+                return NPC_RAZORSCALE_IMAGE;
+            case QUEST_IGNIS_THE_FURNACE_MASTER_MUST_DIE:
+                return NPC_IGNIS_THE_FURNACE_MASTER_IMAGE;
+            case QUEST_XT_002_DECONSTRUCTOR_MUST_DIE:
+                return NPC_XT_002_DECONSTRUCTOR_IMAGE;
+            case QUEST_LORD_JARAXXUS_MUST_DIE:
+                return NPC_LORD_JARAXXUS_IMAGE;
+            default: // case QUEST_LORD_MARROWGAR_MUST_DIE:
+                return NPC_LORD_MARROWGAR_IMAGE;
             }
         }
 
         void JustSummoned(Creature* image) override
         {
-            // xinef: screams like a baby
             if (image->GetEntry() != NPC_ANUBREKHAN_IMAGE)
                 image->SetUnitMovementFlags(MOVEMENTFLAG_RIGHT);
             _summonGUID = image->GetGUID();
         }
+
 
         void UpdateAI(uint32 diff) override
         {
@@ -244,14 +355,16 @@ public:
             {
                 _switchImageTimer = 0;
                 QuestRelationBounds objectQR = sObjectMgr->GetCreatureQuestRelationBounds(me->GetEntry());
+
                 for (QuestRelations::const_iterator i = objectQR.first; i != objectQR.second; ++i)
                 {
                     uint32 questId = i->second;
                     Quest const* quest = sObjectMgr->GetQuestTemplate(questId);
+
                     if (!quest || !quest->IsWeekly())
                         continue;
 
-                    uint32 newEntry = GetImageEntry(questId);
+                    uint32 newEntry = GetWeeklyImageEntry(questId);
                     if (_summonGUID.GetEntry() != newEntry)
                     {
                         if (Creature* image = ObjectAccessor::GetCreature(*me, _summonGUID))
@@ -269,11 +382,11 @@ public:
         uint32 _switchImageTimer;
         ObjectGuid _summonGUID;
     };
-
 };
 
 void AddSC_npc_archmage_timear()
 {
     new npc_archmage_timear();
-    new npc_archmage_landalock_ipp();
+    new npc_archmage_landalock_ipp_3_3();
+    new npc_archmage_landalock_ipp_3_4();
 }
